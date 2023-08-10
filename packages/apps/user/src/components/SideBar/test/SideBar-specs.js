74695f0d30479b4ef9bca0877772a12d443841cd
hwna00 <layton3534@gmail.com>
2023-08-10 11:12:16 +0900

    Test: SideBar 컴포넌트 리팩토링
    - react-router-dom v6를 사용하여 Jest를 사용하는데 문제가 발생
    - 컴포넌트를 RouterProvider로 감싼 후 render 하도록 변경
    - beforeEach를 사용하여 코드 반복 제거

--- packages/apps/user/src/components/SideBar/test/SideBar-specs.js
+++ packages/apps/user/src/components/SideBar/test/SideBar-specs.js
@@ -1,6 +1,9 @@
 import { describe, expect, it } from '@jest/globals';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import SideBar from '../SideBar';
+import { RouterProvider } from 'react-router-dom';
+import router from '../../../router';
 
 const menuNames = [
   '메인 화면',
@@ -10,16 +13,24 @@ const menuNames = [
   '복약 관리',
 ];
 
+//TODO: 로그인 하지 않은 경우 기본 프로필을 보여준다.
+//TODO: 로그인 했지만 이미지가 없는 경우, 기본 프로필을 보여준다.
+//TODO: 로그인했고 이미지가 있다면, 기본 아이콘 대신 이미지를 보여준다.
 describe('Test SideBar Component', () => {
+  beforeEach(() => {
+    render(
+      <RouterProvider router={router}>
+        <SideBar />
+      </RouterProvider>,
+    );
+  });
   it('프로필 생성', () => {
-    render(<SideBar />);
-    const profileButton = screen.getByRole('button', { name: /.*님/i });
+    const profileButton = screen.getByText(/.*님/i);
     expect(profileButton).toBeInTheDocument();
     expect(profileButton).tohave;
   });
 
   it('메뉴 항목 생성', () => {
-    render(<SideBar />);
     menuNames.forEach(menuName => {
       const menuItem = screen.getByText(menuName);
       expect(menuItem).toBeInTheDocument();
@@ -27,7 +38,6 @@ describe('Test SideBar Component', () => {
   });
 
   it('세팅 버튼 생성', () => {
-    render(<SideBar />);
     const settingButton = screen.getByRole('button', { name: /환경 설정/i });
     expect(settingButton).toBeInTheDocument();
   });
