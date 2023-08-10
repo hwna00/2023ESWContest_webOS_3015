4806db57167342d322f9fb8548709f8d7be309a3
1004jaein <jsja031017@kookmin.ac.kr>
2023-08-10 15:56:12 +0900

    Style : css 파일 삭제 후 chakra 적용 - css 내용을 chakra를 이용해서 재설정 후 css 파일 삭제

--- packages/apps/user/src/views/NotFound/NotFound.js
+++ packages/apps/user/src/views/NotFound/NotFound.js
@@ -1,20 +1,31 @@
-import { Button, Stack } from '@chakra-ui/react';
-import React from 'react';
-import './NotFound.css';
+import { Flex, Heading, Stack, Button } from '@chakra-ui/react';
+import { Link as ReactRouterLink } from 'react-router-dom';
 
 function NotFound() {
   return (
-    <div className="notfound">
-      <h1>404 Not Found</h1>
+    <Flex
+      direction="column"
+      align="center"
+      justify="center"
+      minH="100vh"
+      bgColor="#f0f0f0"
+    >
+      <Heading as="h1" fontSize="3rem" mb="2rem" color="#333">
+        404 Not Found
+      </Heading>
+      <Heading as="h2" fontSize="1.5rem" mb="1rem" color="#333">
+        페이지를 찾을 수 없습니다.
+      </Heading>
+
       <Stack direction="row" spacing={4} align="center">
-        <Button colorScheme="blue" onClick={() => window.history.back()}>
-          이전 페이지로
-        </Button>
-        <Button colorScheme="blue" onClick={() => (window.location.href = '/')}>
-          메인 페이지로
-        </Button>
+        <ReactRouterLink to="">
+          <Button colorScheme="blue">이전 페이지로</Button>
+        </ReactRouterLink>
+        <ReactRouterLink to="/">
+          <Button colorScheme="blue">메인 페이지로</Button>
+        </ReactRouterLink>
       </Stack>
-    </div>
+    </Flex>
   );
 }
 
