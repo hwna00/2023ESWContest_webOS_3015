d1cd8077398a5d391f3529b32c9a00ce670db778
hwna00 <layton3534@gmail.com>
2023-08-12 12:50:31 +0900

    Feat: 라우터에 basename 추가
    - createBrowserRouer의 옵션으로 basename을 전달
    - 회원가입 및 로그인 페이지를 담당할 /auth 경로 추가

--- packages/apps/user/src/router.js
+++ packages/apps/user/src/router.js
@@ -1,44 +1,60 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { Outlet, createBrowserRouter } from 'react-router-dom';
 import NotFound from './views/NotFound';
 import Root from './components/Root/Root';
 import MyPage from './views/MyPage/MyPage';
+import SignUp from './views/Auth/SignUp/SignUp';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Root />,
+      errorElement: <NotFound />,
+      children: [
+        {
+          path: '',
+          element: '',
+        },
+        {
+          path: 'mypage',
+          element: <MyPage />,
+        },
+        {
+          path: 'doctor-appointment',
+          element: '',
+        },
+        {
+          path: 'appointment-history',
+          element: '',
+        },
+        {
+          path: 'health',
+          element: '',
+        },
+        {
+          path: 'medicines',
+          element: '',
+        },
+        {
+          path: 'settings',
+          element: '',
+        },
+      ],
+    },
+    {
+      path: 'auth',
+      element: <Outlet />,
+      children: [
+        {
+          path: 'sign-up',
+          element: <SignUp />,
+        },
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <Root />,
-    errorElement: <NotFound />,
-    children: [
-      {
-        path: '',
-        element: '',
-      },
-      {
-        path: 'mypage',
-        element: <MyPage />,
-      },
-      {
-        path: 'doctor-appointment',
-        element: '',
-      },
-      {
-        path: 'appointment-history',
-        element: '',
-      },
-      {
-        path: 'health',
-        element: '',
-      },
-      {
-        path: 'medicines',
-        element: '',
-      },
-      {
-        path: 'settings',
-        element: '',
-      },
-    ],
+    basename: '/',
   },
-]);
+);
 
 export default router;
