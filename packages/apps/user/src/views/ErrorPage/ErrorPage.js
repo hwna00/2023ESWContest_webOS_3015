2938f4f9c64aa93a68144d2243b4c006c1c3e264
1004jaein <jsja031017@kookmin.ac.kr>
2023-08-12 11:42:33 +0900

    Feat  : 500 페이지 테스트 후 완료 - 테스트 코드 작성 후 통과 - 500 페이지 완성

---
+++ packages/apps/user/src/views/ErrorPage/ErrorPage.js
@@ -0,0 +1,33 @@
+import { Link as ReactRouterLink } from 'react-router-dom';
+import { Link as ChakraLink } from '@chakra-ui/react';
+import { Flex, Heading, Stack, Button } from '@chakra-ui/react';
+
+function ErrorPage() {
+  return (
+    <Flex
+      direction="column"
+      align="center"
+      justify="center"
+      minH="100vh"
+      bgColor="#f0f0f0"
+    >
+      <Heading as="h1" fontSize="3rem" mb="2rem" color="#333">
+        500 Server Error
+      </Heading>
+      <Heading as="h2" fontSize="1.5rem" mb="1rem" color="#333">
+        서버에 에러가 발생하였습니다. 잠시 후 다시 시도해주세요.
+      </Heading>
+
+      <Stack direction="row" spacing={4} align="center">
+        <ChakraLink as={ReactRouterLink} onClick={() => window.history.back()}>
+          <Button colorScheme="blue">이전 페이지로</Button>
+        </ChakraLink>
+        <ChakraLink as={ReactRouterLink} to="/">
+          <Button colorScheme="blue">메인 페이지로</Button>
+        </ChakraLink>
+      </Stack>
+    </Flex>
+  );
+}
+
+export default ErrorPage;
---
+++ packages/apps/user/src/views/ErrorPage/test/ErrorPage-specs.js
@@ -0,0 +1,41 @@
+import { BrowserRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import ErrorPage from '../ErrorPage';
+
+describe('Error Page', () => {
+  test('Error Page', () => {
+    render(
+      <BrowserRouter>
+        <ErrorPage />
+      </BrowserRouter>,
+    );
+
+    const msg = screen.getByText(/500 Server Error/i);
+    expect(msg).toBeInTheDocument();
+  });
+  test('이전 버튼', () => {
+    render(
+      <BrowserRouter>
+        <ErrorPage />
+      </BrowserRouter>,
+    );
+
+    const previousBtn = screen.getByRole('button', {
+      name: /이전 페이지로/i,
+    });
+    expect(previousBtn).toBeInTheDocument();
+  });
+  test('메인 버튼', () => {
+    render(
+      <BrowserRouter>
+        <ErrorPage />
+      </BrowserRouter>,
+    );
+
+    const mainBtn = screen.getByRole('button', {
+      name: /메인 페이지로/i,
+    });
+    expect(mainBtn).toBeInTheDocument();
+  });
+});
