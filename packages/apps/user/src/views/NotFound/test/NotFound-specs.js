4b26a9ade6b97362d0af966a5092a21551ceb394
1004jaein <jsja031017@kookmin.ac.kr>
2023-08-12 11:30:20 +0900

    Style : import 순서 변경 및 코드 스타일 변경 - 중요도에 따른 import 요소 순서 변경 - 코드 길이 감소를 위한 스타일 변경

--- packages/apps/user/src/views/NotFound/test/NotFound-specs.js
+++ packages/apps/user/src/views/NotFound/test/NotFound-specs.js
@@ -1,25 +1,17 @@
-import { render, screen } from '@testing-library/react';
-import NotFound from '../NotFound';
-import '@testing-library/jest-dom/extend-expect';
 import { BrowserRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import NotFound from '../NotFound';
 
 describe('NotFound', () => {
   test('Not Found', () => {
-    render(
-      <BrowserRouter>
-        <NotFound />
-      </BrowserRouter>,
-    );
+    render(<NotFound />, { wrapper: BrowserRouter });
 
     const msg = screen.getByText(/404 not found/i);
     expect(msg).toBeInTheDocument();
   });
   test('이전 버튼', () => {
-    render(
-      <BrowserRouter>
-        <NotFound />
-      </BrowserRouter>,
-    );
+    render(<NotFound />, { wrapper: BrowserRouter });
 
     const previousBtn = screen.getByRole('button', {
       name: /이전 페이지로/i,
@@ -27,11 +19,7 @@ describe('NotFound', () => {
     expect(previousBtn).toBeInTheDocument();
   });
   test('메인 버튼', () => {
-    render(
-      <BrowserRouter>
-        <NotFound />
-      </BrowserRouter>,
-    );
+    render(<NotFound />, { wrapper: BrowserRouter });
 
     const mainBtn = screen.getByRole('button', {
       name: /메인 페이지로/i,
