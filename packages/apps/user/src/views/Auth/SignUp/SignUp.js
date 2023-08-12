782e8d346eb2e139816ab266a46dba84388092ca
hwna00 <layton3534@gmail.com>
2023-08-12 12:58:18 +0900

    Feat: 회원가입 페이지 제작
    - 로그인 화면으로 이동할 수 있는 버튼 추가
    - Stepper 추가
    - 단계에 따라 "이전으로", "다음으로", "가입하기" 버튼 생성

--- packages/apps/user/src/views/Auth/SignUp/SignUp.js
+++ packages/apps/user/src/views/Auth/SignUp/SignUp.js
@@ -1,5 +1,83 @@
+import { Link as ReactRouterLink } from 'react-router-dom';
+import {
+  HStack,
+  Heading,
+  VStack,
+  Link as ChakraLink,
+  Box,
+  useSteps,
+  Stepper,
+  Step,
+  StepIndicator,
+  StepStatus,
+  StepIcon,
+  StepNumber,
+  StepSeparator,
+  FormControl,
+  Input,
+  Button,
+} from '@chakra-ui/react';
+
+const steps = [{}, {}, {}, {}];
+
 const SignUp = function () {
-  return <div>sign up</div>;
+  const { activeStep, goToNext, goToPrevious, isActiveStep, isCompleteStep } =
+    useSteps({
+      index: 0,
+      count: steps.length,
+    });
+
+  return (
+    <VStack height={'100vh'} p={'6'}>
+      <HStack width={'full'} justifyContent={'start'} alignItems={'end'}>
+        <Heading as={'h1'}>Housepital 회원가입</Heading>
+        <ChakraLink as={ReactRouterLink} to={'/auth/login'}>
+          이미 계정이 있으신가요? 로그인
+        </ChakraLink>
+      </HStack>
+      <HStack height={'full'}>
+        <Box height={'full'} py={'12'}>
+          <Stepper
+            index={activeStep}
+            orientation="vertical"
+            height={'full'}
+            gap="0"
+            colorScheme="primary"
+          >
+            {steps.map((step, index) => (
+              <Step key={index}>
+                <StepIndicator>
+                  <StepStatus
+                    complete={<StepIcon />}
+                    incomplete={<StepNumber />}
+                    active={<StepNumber />}
+                  />
+                </StepIndicator>
+
+                <StepSeparator />
+              </Step>
+            ))}
+          </Stepper>
+        </Box>
+        <VStack>
+          <FormControl>
+            <Input variant={'filled'} placeholder="아이디"></Input>
+            <Input variant={'filled'} placeholder="이름"></Input>
+            <Input variant={'filled'} placeholder="비밀번호"></Input>
+            <Input variant={'filled'} placeholder="비밀번호 확인"></Input>
+          </FormControl>
+          {isCompleteStep(0) ? (
+            <Button onClick={goToPrevious}>이전으로</Button>
+          ) : null}
+          {isActiveStep(3) ? (
+            <Button onClick={goToNext}>가입하기</Button>
+          ) : (
+            <Button onClick={goToNext}>다음으로</Button>
+          )}
+        </VStack>
+      </HStack>
+    </VStack>
+  );
 };
 
 export default SignUp;
