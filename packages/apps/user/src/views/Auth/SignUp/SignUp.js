dd3532b68c011468ab9f02351164d66f1347ee56
hwna00 <layton3534@gmail.com>
2023-08-14 00:19:25 +0900

    Feat: 회원가입 폼에 validation 적용
    - 회원가입 시 입력해야 하는 정보를 validation 하는 기능 추가
    - 애니메이션 적용을 위해 SignUpForm 컴포넌트를 다시 SignUp view에서 구현
    
    ref: #7

--- packages/apps/user/src/views/Auth/SignUp/SignUp.js
+++ packages/apps/user/src/views/Auth/SignUp/SignUp.js
@@ -15,9 +15,19 @@ import {
   StepSeparator,
   Button,
   StepNumber,
+  FormControl,
+  Input,
+  FormLabel,
+  Textarea,
+  InputGroup,
+  InputLeftAddon,
+  Select,
+  InputRightAddon,
+  FormErrorMessage,
+  ButtonGroup,
 } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
-import SignUpForm from '../../../components/SignUpForm/SignUpForm';
+import { useForm } from 'react-hook-form';
 
 //Todo: 어케 바꾸지..?
 const steps = [{}, {}, {}, {}];
@@ -28,6 +38,14 @@ const SignUp = function () {
       index: 0,
       count: steps.length,
     });
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    watch,
+    formState: { errors },
+  } = useForm({ mode: 'all' });
+
   const [formPosition, setFormPosition] = useState(0);
 
   const handleNext = function () {
@@ -39,10 +57,10 @@ const SignUp = function () {
     goToPrevious();
     setFormPosition(formPosition + 800);
   };
-
   const onSubmit = function (data) {
-    console.log('hi');
+    console.log('submit', data);
   };
+  console.log(watch());
 
   return (
     <VStack height={'100vh'} p={'6'}>
@@ -54,7 +72,7 @@ const SignUp = function () {
       </HStack>
 
       <HStack height={'full'} width={'full'} px={'12'} py={'6'} gap={'12'}>
-        <Box height={'full'} py={'12'}>
+        <Box height={'full'} py={'8'}>
           <Stepper
             as={motion.div}
             index={activeStep}
@@ -77,16 +95,283 @@ const SignUp = function () {
           </Stepper>
         </Box>
 
-        <VStack as="form" onSubmit={() => console.log('on submit')} gap={'4'}>
-          <SignUpForm formPosition={formPosition} />
+        <VStack
+          as="form"
+          onSubmit={handleSubmit(onSubmit)}
+          gap={'4'}
+          alignItems={'flex-end'}
+        >
+          <Box height={'full'} width={800} overflowX={'hidden'} p={'2'}>
+            <HStack
+              gap={0}
+              as={motion.div}
+              animate={{ x: formPosition }}
+              transition={'0.1s linear'}
+            >
+              <VStack minWidth={800} gap={'4'} pr={'10'}>
+                <FormControl width={'full'} isRequired isInvalid={errors.email}>
+                  <FormLabel>이메일</FormLabel>
+                  <Input
+                    required
+                    type="email"
+                    placeholder="이메일을 입력해주세요."
+                    {...register('email', {
+                      required: '이 항목은 필수입니다.',
+                      pattern: {
+                        value:
+                          /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
+                        message: '이메일 형식이 아닙니다.',
+                      },
+                      validate: value => console.log(value),
+                    })}
+                  />
+                  <FormErrorMessage>
+                    {errors.email && errors.email.message}
+                  </FormErrorMessage>
+                </FormControl>
 
-          <HStack
-            width={'full'}
-            justifyContent={'flex-end'}
-            alignItems={'center'}
-            gap={'4'}
-          >
-            {isCompleteStep(0) ? (
+                <FormControl
+                  width={'full'}
+                  isRequired
+                  isInvalid={errors.username}
+                >
+                  <FormLabel>이름</FormLabel>
+                  <Input
+                    required
+                    placeholder="성함을 입력해주세요."
+                    {...register('username', {
+                      required: '이 항목은 필수입니다.',
+                    })}
+                  />
+                  <FormErrorMessage>
+                    {errors.username && errors.username.message}
+                  </FormErrorMessage>
+                </FormControl>
+
+                <FormControl
+                  width={'full'}
+                  isRequired
+                  isInvalid={errors.password}
+                >
+                  <FormLabel>비밀번호</FormLabel>
+                  <Input
+                    required
+                    type="password"
+                    placeholder="4자리 이상의 비밀번호를 입력해주세요."
+                    {...register('password', {
+                      required: '이 항목은 필수입니다.',
+                      min: { value: 4, message: '4자리 이상 입력해주세요.' },
+                    })}
+                  />
+                  <FormErrorMessage>
+                    {errors.password && errors.password.message}
+                  </FormErrorMessage>
+                </FormControl>
+
+                <FormControl
+                  width={'full'}
+                  isRequired
+                  isInvalid={errors.checkPassword}
+                >
+                  <FormLabel>비밀번호 확인</FormLabel>
+                  <Input
+                    required
+                    type="password"
+                    placeholder="필수 요소입니다."
+                    {...register('checkPassword', {
+                      required: '이 항목은 필수입니다.',
+                      validate: {
+                        check: checkPassword => {
+                          if (getValues('password') !== checkPassword) {
+                            return '비밀번호가 일치하지 않습니다.';
+                          }
+                        },
+                      },
+                    })}
+                  />
+                  <FormErrorMessage>
+                    {errors.checkPassword && errors.checkPassword.message}
+                  </FormErrorMessage>
+                </FormControl>
+              </VStack>
+
+              <VStack minWidth={800} gap={'4'} pr={'10'}>
+                <FormControl
+                  width={'full'}
+                  isRequired
+                  isInvalid={errors.birthDate}
+                >
+                  <FormLabel>생년월일</FormLabel>
+                  <Input
+                    required
+                    type="date"
+                    placeholder="생년월일을 선택해주세요."
+                    {...register('birthDate', {
+                      required: '이 항목은 필수입니다.',
+                    })}
+                  />
+                  <FormErrorMessage>
+                    {errors.birthDate && errors.birthDate.message}
+                  </FormErrorMessage>
+                </FormControl>
+
+                <FormControl
+                  width={'full'}
+                  isRequired
+                  isInvalid={errors.address}
+                >
+                  <FormLabel>주소</FormLabel>
+                  <Input
+                    required
+                    placeholder="옵션 요소입니다."
+                    {...register('address', {
+                      required: '이 항목은 필수입니다.',
+                    })}
+                  />
+                  <FormErrorMessage>
+                    {errors.address && errors.address.message}
+                  </FormErrorMessage>
+                </FormControl>
+
+                <FormControl
+                  width={'full'}
+                  isRequired
+                  isInvalid={errors.phoneNumber}
+                >
+                  <FormLabel>전화번호</FormLabel>
+                  <InputGroup colorScheme="primary">
+                    <InputLeftAddon>010</InputLeftAddon>
+                    <Input
+                      required
+                      type="tel"
+                      maxLength={'8'}
+                      placeholder="필수 요소입니다."
+                      {...register('phoneNumber', {
+                        required: '이 항목은 필수입니다.',
+                        pattern: {
+                          value: /^[0-9]{8}$/i,
+                          message:
+                            '전화번호는 8자리의 숫자로만 이루어져야 합니다.',
+                        },
+                      })}
+                    />
+                  </InputGroup>
+                  <FormErrorMessage>
+                    {errors.phoneNumber && errors.phoneNumber.message}
+                  </FormErrorMessage>
+                </FormControl>
+
+                <FormControl
+                  width={'full'}
+                  isInvalid={errors.secondPhoneNumber}
+                >
+                  <FormLabel>비상 시 연락처</FormLabel>
+                  <InputGroup>
+                    <InputLeftAddon children="010" />
+                    <Input
+                      type="tel"
+                      maxLength={'8'}
+                      placeholder="옵션 요소입니다."
+                      {...register('secondPhoneNumber', {
+                        pattern: {
+                          value: /^[0-9]{8}$/i,
+                          message:
+                            '전화번호는 8자리의 숫자로만 이루어져야 합니다.',
+                        },
+                      })}
+                    />
+                  </InputGroup>
+                  <FormErrorMessage>
+                    {errors.secondPhoneNumber &&
+                      errors.secondPhoneNumber.message}
+                  </FormErrorMessage>
+                </FormControl>
+              </VStack>
+
+              <VStack minWidth={800} gap={'4'} pr={'10'}>
+                <HStack width={'full'} gap={'4'}>
+                  <FormControl width={'full'} isInvalid={errors.bloodType}>
+                    <FormLabel>혈액형</FormLabel>
+                    <Select placeholder="혈액형" {...register('bloodType')}>
+                      <option value="blood_A">A형</option>
+                      <option value="blood_B">B형</option>
+                      <option value="blood_O">O형</option>
+                      <option value="blood_AB">AB형</option>
+                    </Select>
+                    <FormErrorMessage>
+                      {errors.bloodType && errors.bloodType.message}
+                    </FormErrorMessage>
+                  </FormControl>
+
+                  <FormControl width={'full'} isInvalid={errors.height}>
+                    <FormLabel>키</FormLabel>
+                    <InputGroup>
+                      <Input
+                        type="number"
+                        placeholder="키"
+                        {...register('height', {
+                          pattern: {
+                            value: /^[0-9]/i,
+                            message: '키는 숫자로만 이루어져야 합니다.',
+                          },
+                        })}
+                      />
+                      <InputRightAddon children="cm" />
+                    </InputGroup>
+                    <FormErrorMessage>
+                      {errors.height?.message}
+                    </FormErrorMessage>
+                  </FormControl>
+
+                  <FormControl width={'full'}>
+                    <FormLabel>몸무게</FormLabel>
+                    <InputGroup>
+                      <Input
+                        type="number"
+                        placeholder="몸무게"
+                        {...register('weight')}
+                      />
+                      <InputRightAddon children="kg" />
+                    </InputGroup>
+                    <FormErrorMessage>
+                      {errors.weight && errors.weight.message}
+                    </FormErrorMessage>
+                  </FormControl>
+                </HStack>
+
+                <FormControl width={'full'} isInvalid={errors.chronicDisease}>
+                  <FormLabel>질환 정보</FormLabel>
+                  <Textarea
+                    resize={'none'}
+                    placeholder="평소 앓고 있었던 질병이 있다면 입력해주세요."
+                    {...register('chronicDisease')}
+                  />
+                  <FormErrorMessage>
+                    {errors.chronicDisease && errors.chronicDisease.message}
+                  </FormErrorMessage>
+                </FormControl>
+
+                <FormControl width={'full'} isInvalid={errors.regularMedicines}>
+                  <FormLabel>복약 정보</FormLabel>
+                  <Textarea
+                    resize={'none'}
+                    placeholder="평소 지속적으로 복용하고 있는 약이 있다면 입력해주세요."
+                    {...register('regularMedicines')}
+                  />
+                </FormControl>
+                <FormErrorMessage>
+                  {errors.regularMedicines && errors.regularMedicines.message}
+                </FormErrorMessage>
+              </VStack>
+
+              <VStack minWidth={800} gap={'4'} pr={'10'}>
+                필요 프로필 사진 추가 //Todo: 사진 찍기
+              </VStack>
+            </HStack>
+          </Box>
+
+          <ButtonGroup gap={'4'}>
+            {isCompleteStep(0) && (
               <Button
                 onClick={handlePrev}
                 colorScheme="primary"
@@ -94,7 +379,7 @@ const SignUp = function () {
               >
                 이전으로
               </Button>
-            ) : null}
+            )}
             {isActiveStep(3) ? (
               <Button
                 type="submit"
@@ -108,7 +393,7 @@ const SignUp = function () {
                 다음으로
               </Button>
             )}
-          </HStack>
+          </ButtonGroup>
         </VStack>
       </HStack>
     </VStack>
