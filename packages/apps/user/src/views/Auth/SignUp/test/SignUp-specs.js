939b266fc631c4ab7c718b6807b2e2f800c57c30
hwna00 <layton3534@gmail.com>
2023-08-15 23:18:41 +0900

    Test: SignUp 페이지 테스트코드 업데이트
    - 회원가입 스텝을 이동하면서 trigger를 추가했기 때문에 기존 테스트 코드는 실행 불가함
    - act와 waitFor를 사용하여 테스트 코드를 수정

--- packages/apps/user/src/views/Auth/SignUp/test/SignUp-specs.js
+++ packages/apps/user/src/views/Auth/SignUp/test/SignUp-specs.js
@@ -1,33 +1,87 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import { BrowserRouter } from 'react-router-dom';
 import SignUp from '../SignUp';
 
+const testForm = {
+  email: 'test@test.com',
+  username: '하철환',
+  password: '121212',
+  checkPassword: '121212',
+  birthDate: '2000-08-03',
+  address: '주소',
+  phoneNumber: '12345678',
+};
+
 describe('Test SignUp Page', () => {
-  beforeEach(() => {
-    render(<SignUp />, { wrapper: BrowserRouter });
+  beforeEach(async () => {
+    act(() => {
+      render(<SignUp />, { wrapper: BrowserRouter });
+    });
   });
 
-  test('로그인화면으로 넘어가는 버튼이 존재하는가', () => {
+  it('로그인화면으로 넘어가는 버튼이 존재하는가', () => {
     const loginBtn = screen.getByText(/로그인/i);
     expect(loginBtn).toBeInTheDocument();
   });
 
-  test('다음 버튼을 눌렀을 때 "이전으로" 버튼이 생성되는가', async () => {
-    const nextBtn = screen.getByRole('button', { name: /다음으로/i });
-    expect(nextBtn).toBeInTheDocument();
-    await userEvent.click(nextBtn);
-    const prevBtn = screen.getByRole('button', { name: /이전으로/i });
-    expect(prevBtn).toBeInTheDocument();
-  });
+  it('필수 입력 요소를 모두 채웠을 때 정상적으로 최종화면으로 이동하는가', async () => {
+    const email = screen.getByLabelText(/이메일/i);
+    const name = screen.getByLabelText(/이름/i);
+    const password = screen.getByPlaceholderText(/비밀번호/i);
+    const checkPassword = screen.getByLabelText(/비밀번호 확인/i, {
+      exact: true,
+    });
 
-  test('회원가입 마지막 단계에서 "가입하기" 버튼이 생성되는가', async () => {
+    await waitFor(() => userEvent.type(email, testForm.email));
+    await waitFor(() => userEvent.type(name, testForm.username));
+    await waitFor(() => userEvent.type(password, testForm.password));
+    await waitFor(() => userEvent.type(checkPassword, testForm.checkPassword));
+
     const nextBtn = screen.getByRole('button', { name: /다음으로/i });
-    await userEvent.click(nextBtn);
-    await userEvent.click(nextBtn);
-    await userEvent.click(nextBtn);
+    await act(() => {
+      userEvent.click(nextBtn);
+    });
+
+    const birthDate = screen.getByLabelText(/생년월일/i);
+    const address = screen.getByLabelText(/주소/i);
+    const phoneNumber = screen.getByLabelText(/전화번호/i);
+
+    await waitFor(() => userEvent.type(birthDate, testForm.birthDate));
+    await waitFor(() => userEvent.type(address, testForm.address));
+    await waitFor(() => userEvent.type(phoneNumber, testForm.phoneNumber));
+
+    await act(() => userEvent.click(nextBtn));
+    await act(() => userEvent.click(nextBtn));
+
     const submitBtn = screen.getByRole('button', { name: /가입하기/i });
     expect(submitBtn).toBeInTheDocument();
+  });
+
+  it.only('필수 입력 요소를 모두 채우지 않았을 때 에러 메시지가 보이는가', async () => {
+    const nextBtn = screen.getByRole('button', { name: /다음으로/i });
+    await act(() => userEvent.click(nextBtn));
+
+    let errorMsg = screen.getAllByText(/이 항목은 필수입니다./i);
+    expect(errorMsg.length).toBe(4);
+
+    const email = screen.getByLabelText(/이메일/i);
+    const name = screen.getByLabelText(/이름/i);
+    const password = screen.getByPlaceholderText(/비밀번호/i);
+    const checkPassword = screen.getByLabelText(/비밀번호 확인/i, {
+      exact: true,
+    });
+
+    await waitFor(() => userEvent.type(email, testForm.email));
+    await waitFor(() => userEvent.type(name, testForm.username));
+    await waitFor(() => userEvent.type(password, testForm.password));
+    await act(() => userEvent.type(checkPassword, testForm.checkPassword));
+
+    await act(() => userEvent.click(nextBtn));
+
+    await act(() => userEvent.click(nextBtn));
+    errorMsg = screen.getAllByText(/이 항목은 필수입니다./i);
+    expect(errorMsg.length).toBe(3);
   });
 });
