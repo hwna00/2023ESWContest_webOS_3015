20911bf77459733c6855656e087dbcd6a33f4061
hwna00 <layton3534@gmail.com>
2023-08-16 16:56:47 +0900

    Feat(user): 캘린더 보기 화면
    - react-calender 라이브러리 설치
    - 날짜 형식 지정을 위해 dayjs 라이브러리 설치
    - 할 일 객체가 담긴 배열을 임시로 생성
    
    ref: #10

--- packages/apps/user/src/views/MainPage/MainPage.js
+++ packages/apps/user/src/views/MainPage/MainPage.js
@@ -1,5 +1,7 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import dayjs from 'dayjs';
 import {
   Box,
   Button,
@@ -14,6 +16,7 @@ import {
   VStack,
   useDisclosure,
 } from '@chakra-ui/react';
+import './MainPage.css';
 
 const notifications = [
   {
@@ -27,13 +30,87 @@ const notifications = [
   },
 ];
 
+const todos = [
+  {
+    date: '2023-08-16',
+    items: [
+      {
+        id: 1,
+        title: '비타민 먹기',
+        description: '08:30',
+      },
+    ],
+  },
+  {
+    date: '2023-08-18',
+    items: [
+      {
+        id: 1,
+        title: '비타민 먹기',
+        description: '08:30',
+      },
+    ],
+  },
+  {
+    date: '2023-08-21',
+    items: [
+      {
+        id: 1,
+        title: '비타민 먹기',
+        description: '08:30',
+      },
+    ],
+  },
+  {
+    date: '2023-08-30',
+    items: [
+      {
+        id: 1,
+        title: '비타민 먹기',
+        description: '08:30',
+      },
+    ],
+  },
+];
+
 const MainPage = function () {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [date, setDate] = useState(new Date());
   const pushRef = useRef();
+  console.log(date);
 
   return (
     <HStack height={'full'}>
-      <Box width={'60%'}>캘린더</Box>
+      <Box width={'60%'} height={'full'}>
+        <Calendar
+          calendarType="gregory"
+          formatDay={(locale, date) => dayjs(date).format('DD')}
+          prev2Label={null}
+          next2Label={null}
+          maxDetail="month"
+          value={date}
+          onChange={setDate}
+          defaultValue={new Date()}
+          tileContent={({ _, date, view }) => {
+            if (
+              todos.find(todo => todo.date === dayjs(date).format('YYYY-MM-DD'))
+            ) {
+              return (
+                <Box
+                  className="dot"
+                  bgColor={'yellow.500'}
+                  width={'2'}
+                  height={'2'}
+                  borderRadius={'full'}
+                  position={'absolute'}
+                  bottom={'2'}
+                  left={'calc(50% - 4px)'}
+                ></Box>
+              );
+            }
+          }}
+        />
+      </Box>
       <VStack>
         <Button ref={pushRef} colorScheme="primary" onClick={onOpen}>
           푸시 알림
