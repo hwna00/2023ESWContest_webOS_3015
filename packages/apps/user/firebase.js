4ddd0e1645f937d3209e0caee9f485ade4f840f2
1004jaein <jsja031017@kookmin.ac.kr>
2023-08-17 13:34:47 +0900

    Feat: 파이어베이스 인증 추가 - firebase 패키지 설치 - user/firebase.js 설정파일 추가

---
+++ packages/apps/user/firebase.js
@@ -0,0 +1,27 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FB_API_KEY,
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FB_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FB_API_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const signIn = (email, password) => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+const logIn = (email, password) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+export { auth, signIn, logIn };
