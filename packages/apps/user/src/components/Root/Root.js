b493b2fa89737b1bf4a222979fb8b7f37fbd5995
yangjiwoong <wpyang123@naver.com>
2023-08-21 18:29:23 +0900

    Feat(user): 상단바 제작
    - 월,일,요일,시간 정보를 나타내는 상단바 컴포넌트 제작
    
    ref: #25

--- packages/apps/user/src/components/Root/Root.js
+++ packages/apps/user/src/components/Root/Root.js
@@ -1,5 +1,8 @@
+import { useState, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
-import { Box } from '@chakra-ui/react';
+import { Box, HStack, Text } from '@chakra-ui/react';
+import dayjs from 'dayjs';
+import 'dayjs/locale/ko';
 import SideBar from '../SideBar/SideBar';
 import { auth } from '../../../firebase';
 
@@ -9,9 +12,38 @@ const Root = function () {
     profileImg: auth.currentUser?.photoURL,
   };
 
+  dayjs.locale('ko');
+
+  const [currentTime, setCurrentTime] = useState(
+    dayjs().format('MM월 DD일 (ddd) 오후 HH:mm'),
+  );
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentTime(dayjs().format('MM월 DD일 (ddd) 오후 HH:mm'));
+      console.log('mounted');
+    }, 1000);
+
+    return () => {
+      clearInterval(timer);
+      console.log('unmounted');
+    };
+  }, []);
+
   return (
     <>
       {auth.currentUser ? <SideBar user={user} /> : <SideBar />}
+      <HStack
+        justifyContent={'flex-end'}
+        alignItems={'center'}
+        bg={'primary.200'}
+        w={'full'}
+        height={'8'}
+        px={'4'}
+        fontWeight={'bold'}
+      >
+        <Text>{currentTime}</Text>
+      </HStack>
       <Box ml="40" p="6" height={'100vh'}>
         <Outlet />
       </Box>
