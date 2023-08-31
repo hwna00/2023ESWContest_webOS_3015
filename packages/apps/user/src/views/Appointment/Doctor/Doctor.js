9406e44a5ab5eef93a313458ed837cb0817456c1
1004jaein <jsja031017@kookmin.ac.kr>
2023-08-31 14:17:28 +0900

    Feat : 의사 리스트 분리

--- packages/apps/user/src/views/Appointment/Doctor/Doctor.js
+++ packages/apps/user/src/views/Appointment/Doctor/Doctor.js
@@ -17,55 +17,12 @@ import {
 } from '@chakra-ui/react';
 import AppointmentCard from './AppointmentCard';
 import BackButton from '../BackButton';
+import initialDoctorList from './DoctorList';
+
 function Doctor() {
   const goPrev = useCallback(() => {
     window.history.back();
   }, []);
-  const initialDoctorList = [
-    {
-      name: '이현철',
-      specialty: '내과 전문의',
-      field: ['내과', '가정의학과'],
-      rate: '4.4',
-      img: 'https://i.namu.wiki/i/AzUH8U5TcGdNJDN9Fl5zyEsLdL72N-PBsR0OjvAtmHRAwSDIcDwRAfYS5m_X_i0KFlZdmGNkwb5f8D_eC3vTuQ.webp',
-    },
-    {
-      name: '양지웅',
-      specialty: '안과 전문의',
-      field: ['안과'],
-      rate: '4.8',
-      img: 'https://i.namu.wiki/i/AzUH8U5TcGdNJDN9Fl5zyEsLdL72N-PBsR0OjvAtmHRAwSDIcDwRAfYS5m_X_i0KFlZdmGNkwb5f8D_eC3vTuQ.webp',
-    },
-    {
-      name: '김재인',
-      specialty: '내과 전문의',
-      field: ['내과'],
-      rate: '5.0',
-      img: 'https://i.namu.wiki/i/AzUH8U5TcGdNJDN9Fl5zyEsLdL72N-PBsR0OjvAtmHRAwSDIcDwRAfYS5m_X_i0KFlZdmGNkwb5f8D_eC3vTuQ.webp',
-    },
-    {
-      name: '하철환',
-      specialty: '소아과 전문의',
-      field: ['소아과', '내과'],
-      rate: '4.7',
-      img: 'https://i.namu.wiki/i/AzUH8U5TcGdNJDN9Fl5zyEsLdL72N-PBsR0OjvAtmHRAwSDIcDwRAfYS5m_X_i0KFlZdmGNkwb5f8D_eC3vTuQ.webp',
-    },
-
-    {
-      name: '서진형',
-      specialty: '가정의학과 전문의',
-      field: ['가정의학과'],
-      rate: '4.9',
-      img: 'https://i.namu.wiki/i/AzUH8U5TcGdNJDN9Fl5zyEsLdL72N-PBsR0OjvAtmHRAwSDIcDwRAfYS5m_X_i0KFlZdmGNkwb5f8D_eC3vTuQ.webp',
-    },
-    {
-      name: '송보경',
-      specialty: '이비인후과 전문의',
-      field: ['이비인후과'],
-      rate: '4.5',
-      img: 'https://i.namu.wiki/i/AzUH8U5TcGdNJDN9Fl5zyEsLdL72N-PBsR0OjvAtmHRAwSDIcDwRAfYS5m_X_i0KFlZdmGNkwb5f8D_eC3vTuQ.webp',
-    },
-  ];
 
   const specialties = [
     '이비인후과',
