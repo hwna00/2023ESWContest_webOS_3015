96a6bad90ac7f005af51187349cba1422df76338
1004jaein <jsja031017@kookmin.ac.kr>
2023-08-31 14:25:53 +0900

    Feat : 진료 과목 리스트 분리

--- packages/apps/user/src/views/Appointment/Doctor/Doctor.js
+++ packages/apps/user/src/views/Appointment/Doctor/Doctor.js
@@ -18,23 +18,12 @@ import {
 import AppointmentCard from './AppointmentCard';
 import BackButton from '../BackButton';
 import initialDoctorList from './DoctorList';
+import specialties from './Specialties';
 
 function Doctor() {
   const goPrev = useCallback(() => {
     window.history.back();
   }, []);
-
-  const specialties = [
-    '이비인후과',
-    '안과',
-    '내과',
-    '피부과',
-    '정형외과',
-    '성형외과',
-    '산부인과',
-    '가정의학과',
-    '정신과',
-  ];
 
   const [sortBy, setSortBy] = useState();
   const [selectedSpecialties, setSelectedSpecialties] = useState([]);
---
+++ packages/apps/user/src/views/Appointment/Doctor/Specialties.js
@@ -0,0 +1,13 @@
+const specialties = [
+  '이비인후과',
+  '안과',
+  '내과',
+  '피부과',
+  '정형외과',
+  '성형외과',
+  '산부인과',
+  '가정의학과',
+  '정신과',
+];
+
+export default specialties;
