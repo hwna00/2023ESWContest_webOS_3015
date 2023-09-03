05299076328bc8bc89fec19094d48564cc18b407
1004jaein <jsja031017@kookmin.ac.kr>
2023-09-03 15:55:19 +0900

    Feat : 예약카드 컴포넌트 분리

---
+++ packages/apps/user/src/views/Appointment/Doctor/AppointDoctor.js
@@ -0,0 +1,127 @@
+import { useState, useCallback } from 'react';
+import {
+  Box,
+  Flex,
+  Heading,
+  Button,
+  Stack,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Checkbox,
+  CheckboxGroup,
+  SimpleGrid,
+} from '@chakra-ui/react';
+
+import AppointmentCard from './AppointmentCard';
+import BackButton from '../BackButton';
+import initialDoctorList from './DoctorList';
+import specialties from './Specialties';
+
+function AppointDoctor() {
+  const goPrev = useCallback(() => {
+    window.history.back();
+  }, []);
+
+  const [sortBy, setSortBy] = useState();
+  const [selectedSpecialties, setSelectedSpecialties] = useState([]);
+  const [doctorList, setDoctorList] = useState(initialDoctorList);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const handleSortByChange = event => {
+    const newSortBy = event;
+    if (newSortBy !== sortBy) {
+      setSortBy(newSortBy);
+    } else {
+      setDoctorList([...doctorList]);
+    }
+  };
+
+  const handleSpecialtyChange = selectedOptions => {
+    setSelectedSpecialties(selectedOptions);
+  };
+
+  const doctorsToDisplay = doctorList.filter(doctor => {
+    const isSelectedSpecialty = doctor.field.some(specialty =>
+      selectedSpecialties.includes(specialty),
+    );
+    return selectedSpecialties.length === 0 || isSelectedSpecialty;
+  });
+
+  if (sortBy === 'name') {
+    doctorsToDisplay.sort((a, b) => (a.name > b.name ? 1 : -1));
+  } else {
+    doctorsToDisplay.sort((a, b) => (a.rate < b.rate ? 1 : -1));
+  }
+
+  return (
+    <Flex direction="column" alignItems="flex-start">
+      <Box>
+        <BackButton goPrev={goPrev} title={'의사별 보기'} />
+        <Button onClick={onOpen} position="fixed" right="8" top="4">
+          필터적용
+        </Button>
+
+        <Modal isOpen={isOpen} onClose={onClose} size="4xl">
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>필터적용</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+              <Box>
+                <Heading as="h3">정렬 기준</Heading>
+                <Checkbox
+                  id="name"
+                  checked={sortBy === 'name'}
+                  onChange={() => handleSortByChange('name')}
+                >
+                  이름순
+                </Checkbox>
+                <Checkbox
+                  id="rating"
+                  checked={sortBy === 'rating'}
+                  onChange={() => handleSortByChange('rating')}
+                >
+                  별점순
+                </Checkbox>
+              </Box>
+              <Box>
+                <Heading as="h3">진료 과목</Heading>
+                <CheckboxGroup
+                  defaultValue={selectedSpecialties}
+                  onChange={handleSpecialtyChange}
+                >
+                  <Stack spacing={2} direction={['column', 'row']}>
+                    {specialties.map(specialty => (
+                      <Checkbox
+                        name={specialty}
+                        value={specialty}
+                        key={specialty}
+                      >
+                        {specialty}
+                      </Checkbox>
+                    ))}
+                  </Stack>
+                </CheckboxGroup>
+              </Box>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      </Box>
+
+      <Box width={'full'} maxHeight="80vh" overflowY="scroll">
+        <SimpleGrid columns={2} gap="8" mt="4" width={'full'} padding="8">
+          {doctorsToDisplay.map(info => (
+            <AppointmentCard info={info} />
+          ))}
+        </SimpleGrid>
+      </Box>
+    </Flex>
+  );
+}
+
+export default AppointDoctor;
--- packages/apps/user/src/views/Appointment/Doctor/AppointmentCard.js
+++ packages/apps/user/src/views/Appointment/Doctor/AppointmentCard.js
@@ -1,54 +1,42 @@
-import {
-  Box,
-  Flex,
-  Heading,
-  Text,
-  Image,
-  Icon,
-  SimpleGrid,
-} from '@chakra-ui/react';
+import { Box, Flex, Heading, Text, Image, Icon } from '@chakra-ui/react';
 import { StarIcon } from '@chakra-ui/icons';
 
-function AppointmentCard({ doctors }) {
+function AppointmentCard({ info }) {
   return (
-    <SimpleGrid columns={2} gap="8" mt="4" width={'full'} padding="8">
-      {doctors.map(doctor => (
-        <Box
-          key={doctor.name}
-          backgroundColor="primary.300"
-          padding="2"
-          borderRadius="md"
-          width={{ base: 'full', md: 'auto' }}
-          marginBottom="4"
-        >
-          <Flex alignItems="center">
-            <Image
-              src={doctor.img}
-              alt={`${doctor.name} 의사`}
-              width="24"
-              height="24"
-              objectFit="cover"
-              borderRadius="full"
-            />
-            <Box ml={{ base: '2', md: '4' }}>
-              <Heading as="h3" size="md" mb="1">
-                {doctor.name} 의사
-              </Heading>
-              <Text fontSize="sm" mb={1}>
-                {doctor.specialty}
-              </Text>
-              <Text fontSize={'sm'} mb={'2'}>
-                {doctor.field.join(', ')}
-              </Text>
-              <Flex alignItems={'center'}>
-                <Icon as={StarIcon} boxSize={'4'} mr={'2'} />
-                <Text fontSize={'sm'}>{doctor.rate}</Text>
-              </Flex>
-            </Box>
+    <Box
+      key={info.name}
+      backgroundColor="primary.300"
+      padding="2"
+      borderRadius="md"
+      width={{ base: 'full', md: 'auto' }}
+      marginBottom="4"
+    >
+      <Flex alignItems="center">
+        <Image
+          src={info.img}
+          alt={`${info.name} 의사`}
+          width="24"
+          height="24"
+          objectFit="cover"
+          borderRadius="full"
+        />
+        <Box ml={{ base: '2', md: '4' }}>
+          <Heading as="h3" size="md" mb="1">
+            {info.name} 의사
+          </Heading>
+          <Text fontSize="sm" mb={1}>
+            {info.specialty}
+          </Text>
+          <Text fontSize={'sm'} mb={'2'}>
+            {info.field.join(', ')}
+          </Text>
+          <Flex alignItems={'center'}>
+            <Icon as={StarIcon} boxSize={'4'} mr={'2'} />
+            <Text fontSize={'sm'}>{info.rate}</Text>
           </Flex>
         </Box>
-      ))}
-    </SimpleGrid>
+      </Flex>
+    </Box>
   );
 }
 
