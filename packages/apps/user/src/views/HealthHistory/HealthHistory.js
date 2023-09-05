fc7d02ccebb4d705490abbba67ec4b58b58e5cd9
yangjiwoong <wpyang123@naver.com>
2023-09-05 09:35:21 +0900

    Feat(user): 건강 기록 페이지 생성
    - 최근 건강 정보 측정치를 보여주는 건강 기록 페이지 생성
    - 최근 측정치를 배열로 저장해 gird와 map함수 이용하여 보여줌
    
    ref: #21

--- packages/apps/user/src/views/HealthHistory/HealthHistory.js
+++ packages/apps/user/src/views/HealthHistory/HealthHistory.js
@@ -1,5 +1,48 @@
+import { Text, Grid, Flex, VStack, HStack } from '@chakra-ui/react';
+
 const HealthHistory = function () {
-  return <></>;
+  const recentData = [
+    { name: '심박수', measuementTime: '17:00', value: 110 },
+    { name: '체온', measuementTime: '16:00', value: 90 },
+    { name: '수면', measuementTime: '11:00', value: 8 },
+    { name: '혈당', measuementTime: '12:00', value: 80 },
+    { name: '혈압', measuementTime: '23:00', value: 85 },
+    { name: '몸무게', measuementTime: '17:00', value: 60 },
+  ];
+  return (
+    <Flex h={'full '} justifyContent={'center'} alignItems={'center'}>
+      <Grid
+        w={'85%'}
+        templateRows="repeat(2, 1fr)"
+        templateColumns="repeat(3, 1fr)"
+        gap={6}
+        placeItems={'center'}
+      >
+        {recentData.map((data, index) => (
+          <VStack
+            h={'full'}
+            w={'full'}
+            bg={'primary.200'}
+            borderRadius={10}
+            padding={'4'}
+            justifyContent={'space-between'}
+          >
+            <HStack w={'full'} justifyContent={'space-between'}>
+              <Text fontSize="2xl" fontWeight={'bold'}>
+                {data.name}
+              </Text>
+              <Text fontSize="sm" float={'right'} opacity={'0.5'}>
+                {data.measuementTime}&gt;
+              </Text>
+            </HStack>
+            <Text w={'90%'} fontSize="5xl" align={'right'}>
+              {data.value}
+            </Text>
+          </VStack>
+        ))}
+      </Grid>
+    </Flex>
+  );
 };
 
 export default HealthHistory;
