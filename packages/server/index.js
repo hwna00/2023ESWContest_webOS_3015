e2423b8c1975d382e30cae7c1f3aaa6f6157d77b
hwna00 <layton3534@gmail.com>
2023-09-06 20:35:03 +0900

    Feat(server): express와 mysql 서버 연동
    - mysql2 패키지 설치
    
    ref: #39

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const mysql = require('mysql2');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,6 +10,23 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
+
+const dbConfig = {
+  host: process.env.HOST,
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+};
+
+const pool = mysql.createPool(dbConfig);
+
+pool.getConnection(err => {
+  if (err) {
+    console.error('MySQL 연결 실패:', err);
+  } else {
+    console.log('MySQL 연결 성공');
+  }
+});
 
 app.get('/', (req, res) => {
   console.log('get /');
