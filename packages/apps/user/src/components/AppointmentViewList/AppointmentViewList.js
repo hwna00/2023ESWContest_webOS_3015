b1640258d644b5244c3c299d98c15fe5a2c66642
yangjiwoong <wpyang123@naver.com>
2023-09-08 00:10:47 +0900

    Refactor(user): 병원별 보기, 의사별 보기 컴포넌트 분리
    - 병원별 보기, 의사별 보기 컴포넌트 분리
    - type(hospital, doctor)에 따른 information 인자를 컴포넌트에 넘기는 방식으로 분리

---
+++ packages/apps/user/src/components/AppointmentViewList/AppointmentViewList.js
@@ -0,0 +1,70 @@
+import { HStack, VStack, Text, Divider } from '@chakra-ui/react';
+import { Link as ChakraLink } from '@chakra-ui/react';
+import { Link as ReactRouterLink } from 'react-router-dom';
+
+const AppointmentViewList = function ({ type, information }) {
+  return (
+    <VStack w={'full'} h={'full'}>
+      <HStack
+        w={'full'}
+        justifyContent={'space-between'}
+        alignItems={'flex-end'}
+        pl={'2'}
+        pr={'6'}
+      >
+        <Text fontSize={'xl'} fontWeight={'bold'}>
+          {type === 'hospital' ? '병원별 보기' : '의사별 보기'}
+        </Text>
+        {type === 'hospital' ? (
+          <ChakraLink as={ReactRouterLink} to="/appointment/hospitals">
+            + 더보기
+          </ChakraLink>
+        ) : (
+          <ChakraLink as={ReactRouterLink} to="/appointment/doctors">
+            + 더보기
+          </ChakraLink>
+        )}
+      </HStack>
+
+      <VStack w={'full'} h={'80'} overflowY={'auto'}>
+        {information.map(info => (
+          <VStack
+            w={'full'}
+            alignItems={'flex-start'}
+            px={'2'}
+            key={info.hospitalKey}
+          >
+            <Divider w={'full'} h={'0.5'} bgColor={'primary.300'} />
+            <VStack pl={'2'} gap={'0'} alignItems={'flex-start'}>
+              <Text>{info.name}</Text>
+              <HStack gap={'6'}>
+                {info.isOpen ? (
+                  <Text fontSize={'md'} color={'primary.400'}>
+                    진료중
+                  </Text>
+                ) : (
+                  <Text fontSize={'md'} color={'red'}>
+                    영업종료
+                  </Text>
+                )}
+                <Text>
+                  {info.rating.toFixed(1)}&nbsp;&#40;
+                  {info.numberOfRatings}
+                  &#41;
+                </Text>
+              </HStack>
+              <HStack my={'2'} flexWrap={'wrap'} rowGap={'0'} columnGap={'3'}>
+                {info.specialities.map((speciality, index) => (
+                  <Text key={index}>{speciality}</Text>
+                ))}
+              </HStack>
+            </VStack>
+            <Divider w={'full'} h={'0.5'} bgColor={'primary.300'} />
+          </VStack>
+        ))}
+      </VStack>
+    </VStack>
+  );
+};
+
+export default AppointmentViewList;
--- packages/apps/user/src/views/Appointment/Appointment.js
+++ packages/apps/user/src/views/Appointment/Appointment.js
@@ -1,12 +1,5 @@
-import {
-  Box,
-  Flex,
-  HStack,
-  VStack,
-  Text,
-  Image,
-  Divider,
-} from '@chakra-ui/react';
+import { Box, Flex, HStack, VStack, Text, Image } from '@chakra-ui/react';
+import AppointmentViewList from '../../components/AppointmentViewList/AppointmentViewList';
 import { Link as ChakraLink } from '@chakra-ui/react';
 import { Link as ReactRouterLink } from 'react-router-dom';
 
@@ -162,125 +155,8 @@ const Appointment = function () {
         </HStack>
 
         <HStack w={'full'} mt={'4'}>
-          <VStack w={'full'} h={'full'}>
-            <HStack
-              w={'full'}
-              justifyContent={'space-between'}
-              alignItems={'flex-end'}
-              pl={'2'}
-              pr={'6'}
-            >
-              <Text fontSize={'xl'} fontWeight={'bold'}>
-                병원별 보기
-              </Text>
-              <ChakraLink as={ReactRouterLink} to="/appointment/doctors">
-                + 더보기
-              </ChakraLink>
-            </HStack>
-
-            <VStack w={'full'} h={'80'} overflowY={'auto'}>
-              {hospitalInfo.map(info => (
-                <VStack
-                  w={'full'}
-                  alignItems={'flex-start'}
-                  px={'2'}
-                  key={info.hospitalKey}
-                >
-                  <Divider w={'full'} h={'0.5'} bgColor={'primary.300'} />
-                  <VStack pl={'2'} gap={'0'} alignItems={'flex-start'}>
-                    <Text>{info.name}</Text>
-                    <HStack gap={'6'}>
-                      {info.isOpen ? (
-                        <Text fontSize={'md'} color={'primary.400'}>
-                          진료중
-                        </Text>
-                      ) : (
-                        <Text fontSize={'md'} color={'red'}>
-                          영업종료
-                        </Text>
-                      )}
-                      <Text>
-                        {info.rating.toFixed(1)}&nbsp;&#40;
-                        {info.numberOfRatings}
-                        &#41;
-                      </Text>
-                    </HStack>
-                    <HStack
-                      my={'2'}
-                      flexWrap={'wrap'}
-                      rowGap={'0'}
-                      columnGap={'3'}
-                    >
-                      {info.specialities.map((speciality, index) => (
-                        <Text key={index}>{speciality}</Text>
-                      ))}
-                    </HStack>
-                  </VStack>
-                  <Divider w={'full'} h={'0.5'} bgColor={'primary.300'} />
-                </VStack>
-              ))}
-            </VStack>
-          </VStack>
-
-          <VStack w={'full'} h={'full'}>
-            <HStack
-              w={'full'}
-              justifyContent={'space-between'}
-              alignItems={'flex-end'}
-              pl={'2'}
-              pr={'6'}
-            >
-              <Text fontSize={'xl'} fontWeight={'bold'}>
-                의사별 보기
-              </Text>
-              <ChakraLink as={ReactRouterLink} to="/appointment/doctors">
-                + 더보기
-              </ChakraLink>
-            </HStack>
-
-            <VStack w={'full'} h={'80'} overflowY={'auto'}>
-              {doctorInfo.map(info => (
-                <VStack
-                  w={'full'}
-                  alignItems={'flex-start'}
-                  px={'2'}
-                  key={info.hospitalKey}
-                >
-                  <Divider w={'full'} h={'0.5'} bgColor={'primary.300'} />
-                  <VStack pl={'2'} gap={'0'} alignItems={'flex-start'}>
-                    <Text>{info.name}</Text>
-                    <HStack gap={'6'}>
-                      {info.isOpen ? (
-                        <Text fontSize={'md'} color={'primary.400'}>
-                          진료중
-                        </Text>
-                      ) : (
-                        <Text fontSize={'md'} color={'red'}>
-                          영업종료
-                        </Text>
-                      )}
-                      <Text>
-                        {info.rating.toFixed(1)}&nbsp;&#40;
-                        {info.numberOfRatings}
-                        &#41;
-                      </Text>
-                    </HStack>
-                    <HStack
-                      my={'2'}
-                      flexWrap={'wrap'}
-                      rowGap={'0'}
-                      columnGap={'3'}
-                    >
-                      {info.specialities.map((speciality, index) => (
-                        <Text key={index}>{speciality}</Text>
-                      ))}
-                    </HStack>
-                  </VStack>
-                  <Divider w={'full'} h={'0.5'} bgColor={'primary.300'} />
-                </VStack>
-              ))}
-            </VStack>
-          </VStack>
+          <AppointmentViewList type={'hospital'} information={hospitalInfo} />
+          <AppointmentViewList type={'doctor'} information={doctorInfo} />
         </HStack>
       </VStack>
     </>
