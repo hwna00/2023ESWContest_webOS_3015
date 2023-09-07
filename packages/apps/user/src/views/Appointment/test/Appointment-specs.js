0e7da15c9700aff3072a2c40a771fa8bec714875
yangjiwoong <wpyang123@naver.com>
2023-09-08 00:21:54 +0900

    Test(Fix): test코드 수정
    - Appointment 페이지 테스트 코드 수정
    - 종류별 보기 컴포넌트 분리를 위한 테스트 코드 수정
    - 컴포넌트 import 위치 수정

--- packages/apps/user/src/views/Appointment/test/Appointment-specs.js
+++ packages/apps/user/src/views/Appointment/test/Appointment-specs.js
@@ -1,31 +1,54 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Appointment from '../Appointment';
+import AppointmentViewList from '../../../components/AppointmentViewList/AppointmentViewList';
+import { BrowserRouter } from 'react-router-dom';
+
+const testHospitalInfo = [
+  {
+    hospitalKey: 0,
+    name: '지웅병원',
+    isOpen: true,
+    rating: 5.0,
+    numberOfRatings: 30,
+    specialities: ['이비인후과', '성형외과'],
+  },
+];
+
+const testDoctorInfo = [
+  {
+    hospitalKey: 0,
+    name: '양지웅',
+    isOpen: true,
+    rating: 5.0,
+    numberOfRatings: 30,
+    specialities: ['이비인후과', '성형외과'],
+  },
+];
 
 test('진행 현황 텍스트가 있다.', () => {
-  render(<Appointment />);
+  render(<Appointment />, { wrapper: BrowserRouter });
   const txtProgress = screen.getByText('진행 현황');
   expect(txtProgress).toBeInTheDocument();
 });
 
 describe('즐겨찾기 컨텐츠가 뜬다', () => {
   beforeEach(() => {
-    render(<Appointment />);
+    render(<Appointment />, { wrapper: BrowserRouter });
   });
 
   it('즐겨찾기 텍스트가 뜬다', () => {
     const txtFavorites = screen.getByText('즐겨찾기');
     expect(txtFavorites).toBeInTheDocument();
   });
-  it('더보기 링크가 뜬다', () => {
-    const btnShowMore = screen.getByRole('Button', { name: '더보기' });
-    expect(btnShowMore).toBeInTheDocument();
-  });
 });
 
 describe('병원별 보기 컨텐츠가 뜬다', () => {
   beforeEach(() => {
-    render(<Appointment />);
+    render(
+      <AppointmentViewList type={'hospital'} information={testHospitalInfo} />,
+      { wrapper: BrowserRouter },
+    );
   });
 
   it('병원별 보기 텍스트가 뜬다', () => {
@@ -33,14 +56,19 @@ describe('병원별 보기 컨텐츠가 뜬다', () => {
     expect(txtHospital).toBeInTheDocument();
   });
   it('더보기 링크가 뜬다', () => {
-    const btnShowMore = screen.getByRole('Button', { name: '더보기' });
-    expect(btnShowMore).toBeInTheDocument();
+    const btnShowMore = screen.getByRole('link', { name: '+ 더보기' });
+    expect(btnShowMore).toHaveAttribute('href', '/appointment/hospitals');
   });
 });
 
 describe('의사별 보기 컨텐츠가 뜬다', () => {
   beforeEach(() => {
-    render(<Appointment />);
+    render(
+      <AppointmentViewList type={'doctor'} information={testDoctorInfo} />,
+      {
+        wrapper: BrowserRouter,
+      },
+    );
   });
 
   it('의사별 보기 텍스트가 뜬다', () => {
@@ -48,7 +76,7 @@ describe('의사별 보기 컨텐츠가 뜬다', () => {
     expect(txtDoctor).toBeInTheDocument();
   });
   it('더보기 링크가 뜬다', () => {
-    const btnShowMore = screen.getByRole('Button', { name: '더보기' });
-    expect(btnShowMore).toBeInTheDocument();
+    const btnShowMore = screen.getByRole('link', { name: '+ 더보기' });
+    expect(btnShowMore).toHaveAttribute('href', '/appointment/doctors');
   });
 });
