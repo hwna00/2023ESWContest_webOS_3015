4abc9cd3da5142441040a83a593b20e976cace1a
1004jaein <jsja031017@kookmin.ac.kr>
2023-09-07 10:47:21 +0900

    Feat : 대기실 페이지 제작
    ref : #17

---
+++ packages/apps/user/src/views/Appointment/WaitingRoom/WaitingItem.js
@@ -0,0 +1,27 @@
+import { Box, HStack, Button } from '@chakra-ui/react';
+
+const WaitingItem = function ({ waiting, cancelAppointment, index }) {
+  return (
+    <HStack
+      bg={'primary.100'}
+      py={'4'}
+      borderRadius={'10'}
+      justifyContent={'space-evenly'}
+    >
+      <Box w={'48'} textAlign={'center'}>
+        {waiting.date}
+      </Box>
+      <Box w={'48'} textAlign={'center'}>
+        {waiting.hospital}
+      </Box>
+      <Box w={'48'} textAlign={'center'}>
+        {waiting.name}
+      </Box>
+      <Box w={'20'} textAlign={'center'}>
+        <Button onClick={() => cancelAppointment(index)}>취소하기</Button>
+      </Box>
+    </HStack>
+  );
+};
+
+export default WaitingItem;
