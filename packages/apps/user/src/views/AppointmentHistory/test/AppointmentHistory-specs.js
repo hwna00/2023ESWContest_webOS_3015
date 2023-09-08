98aaf6b3148ce8d8aaa7eb95ced1b358f7e8d64c
yangjiwoong <wpyang123@naver.com>
2023-09-08 18:43:57 +0900

    Fix(user): 진료 내역 페이지 테스트 코드 수정
    - AppointmentHistory 테스트 코드 수정
    - eslint 방식에 따라, beforeEach에서 컨포넌트를 렌더링 하던 방식에서 각 it 함수 안에서 렌더링 하는 방식으로 변경

--- packages/apps/user/src/views/AppointmentHistory/test/AppointmentHistory-specs.js
+++ packages/apps/user/src/views/AppointmentHistory/test/AppointmentHistory-specs.js
@@ -3,23 +3,23 @@ import '@testing-library/jest-dom';
 import AppointmentHistory from '../AppointmentHistory';
 
 describe('각 항목이 뜬다', () => {
-  beforeEach(() => {
-    render(<AppointmentHistory />);
-  });
-
   it('처방 일시가 뜬다', () => {
+    render(<AppointmentHistory />);
     const txtPrescriptionDate = screen.getByText('처방 일시');
     expect(txtPrescriptionDate).toBeInTheDocument();
   });
   it('진료 병원이 뜬다', () => {
+    render(<AppointmentHistory />);
     const txtHospitalName = screen.getByText('진료 병원');
     expect(txtHospitalName).toBeInTheDocument();
   });
   it('제조 약국이 뜬다', () => {
+    render(<AppointmentHistory />);
     const txtPharmacyName = screen.getByText('제조 약국');
     expect(txtPharmacyName).toBeInTheDocument();
   });
   it('즐겨찾기가 뜬다', () => {
+    render(<AppointmentHistory />);
     const txtFavorites = screen.getByText('즐겨찾기');
     expect(txtFavorites).toBeInTheDocument();
   });
