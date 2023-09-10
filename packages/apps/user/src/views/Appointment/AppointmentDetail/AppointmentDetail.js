1ba2c9959cd9a567a77635e0ae5f0d51c63d8e99
hwna00 <layton3534@gmail.com>
2023-09-10 18:30:12 +0900

    Feat(user): 의사 상세 페이지에서 데이터 불러오기
    - 백엔드 api가 아직 없으므로 DataList 파일에서 의사 정보를 불러오는 기능 추가
    - useParam 훅을 사용하여 현재 라우트에 해당하는 의사의 id를 불러오는 기능 추가
    - 받아온 정보는 useState 훅을 사용하여 저장
    
    ref: #14

--- packages/apps/user/src/views/Appointment/AppointmentDetail/AppointmentDetail.js
+++ packages/apps/user/src/views/Appointment/AppointmentDetail/AppointmentDetail.js
@@ -1,7 +1,17 @@
-import { Text } from '@chakra-ui/react';
+import { Box, HStack, Image, Tag, Text, VStack } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { DoctorList } from '../dataList';
 
 const AppointmentDetail = function () {
-  return <Text>Appointment Detail</Text>;
+  const { id } = useParams();
+  const [doctor, setDoctor] = useState({});
+  useEffect(() => {
+    const found = DoctorList.find(d => d?.id === id);
+    setDoctor(found);
+  }, []);
+
+  return <HStack height={'full'}></HStack>;
 };
 
 export default AppointmentDetail;
