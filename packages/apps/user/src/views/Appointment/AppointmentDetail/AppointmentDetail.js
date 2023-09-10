5b64a50b6b9357f129fc81103e35d78460ae0fe2
hwna00 <layton3534@gmail.com>
2023-09-10 18:32:17 +0900

    Feat(user): 의사 상세 페이지에 뒤로가기 버튼 추가

--- packages/apps/user/src/views/Appointment/AppointmentDetail/AppointmentDetail.js
+++ packages/apps/user/src/views/Appointment/AppointmentDetail/AppointmentDetail.js
@@ -2,6 +2,7 @@ import { Box, HStack, Image, Tag, Text, VStack } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { DoctorList } from '../dataList';
+import BackButton from '../../../components/BackButton/BackButton';
 
 const AppointmentDetail = function () {
   const { id } = useParams();
@@ -11,7 +12,17 @@ const AppointmentDetail = function () {
     setDoctor(found);
   }, []);
 
-  return <HStack height={'full'}></HStack>;
+  return (
+    <HStack height={'full'}>
+      <VStack
+        height={'full'}
+        justifyContent={'flex-start'}
+        alignItems={'center'}
+      >
+        <BackButton />
+      </VStack>
+    </HStack>
+  );
 };
 
 export default AppointmentDetail;
