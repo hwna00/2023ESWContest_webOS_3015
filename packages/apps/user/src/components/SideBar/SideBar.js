f2be7f150fa2c3052c37178fceb799e2ef5d2262
hwna00 <layton3534@gmail.com>
2023-09-12 19:59:16 +0900

    Feat(user): 사이드바 구조 변경
    - 기존: 버튼 목록을 리스트에 넣어 반복문을 통해 렌더링
    - 변경: Tabs과 TabList를 사용하여 렌더링
    
    ref: [FEAT] 사이드바 목록 선택 유지 기능 #57

--- packages/apps/user/src/components/SideBar/SideBar.js
+++ packages/apps/user/src/components/SideBar/SideBar.js
@@ -10,6 +10,12 @@ import {
   Flex,
   SkeletonCircle,
   SkeletonText,
+  Tabs,
+  TabList,
+  Tab,
+  TabPanels,
+  TabPanel,
+  Box,
 } from '@chakra-ui/react';
 
 const menuNames = [
@@ -21,107 +27,169 @@ const menuNames = [
 ];
 
 // TODO: 버튼 클릭 시, 배경 색상이 유지되는 기능을 추가해야 한다.
-const SideBar = function ({ user }) {
+// const SideBar = function ({ user }) {
+//   return (
+//     <Flex
+//       height="100vh"
+//       width="40"
+//       color="white"
+//       bgColor="primary.500"
+//       py="4"
+//       direction="column"
+//       justifyContent="space-between"
+//       alignItems="center"
+//       borderRightRadius="md"
+//       position="absolute"
+//       top="0"
+//       left="0"
+//       zIndex={2}
+//     >
+//       <VStack width="full" py="2">
+//         {user ? (
+//           <ChakraLink
+//             as={ReactRouterLink}
+//             to="/mypage"
+//             textDecorationLine="none"
+//             _hover={{ textDecoration: 'none' }}
+//           >
+//             <Avatar src={user?.photoURL} size="xl" />
+//             <Text
+//               mt={2}
+//               fontSize="xl"
+//               fontWeight="bold"
+//               textAlign="center"
+//             >
+//               {user?.displayName}님
+//             </Text>
+//           </ChakraLink>
+//         ) : (
+//           <>
+//             <SkeletonCircle size="24" />
+//             <SkeletonText
+//               mt={2}
+//               skeletonHeight="5"
+//               width="16"
+//               noOfLines={1}
+//             />
+//           </>
+//         )}
+//       </VStack>
+
+//       <VStack width="full" gap={0}>
+//         {menuNames.map(menu => (
+//             <ChakraLink
+//               as={ReactRouterLink}
+//               to={menu.path}
+//               width="full"
+//               key={menu.name}
+//             >
+//               <Button
+//                 width="full"
+//                 size="lg"
+//                 borderRadius="none"
+//                 color="white"
+//                 bgColor="primary.500"
+//                 fontSize="xl"
+//                 _hover={{
+//                   bgColor: 'primary.700',
+//                 }}
+//                 _focus={{
+//                   bgColor: 'white',
+//                   color: 'primary.500',
+//                   fontWeight: 'bold',
+//                 }}
+//               >
+//                 {menu.name}
+//               </Button>
+//             </ChakraLink>
+//           ))}
+//       </VStack>
+//       <HStack width="full">
+//         <ChakraLink as={ReactRouterLink} to="/" width="full">
+//           <Button
+//             leftIcon={<FaCog />}
+//             variant="ghost"
+//             width="full"
+//             size="lg"
+//             borderRadius="none"
+//             color="white"
+//             bgColor="primary.500"
+//             _hover={{
+//               bgColor: 'primary.700',
+//             }}
+//             _focus={{
+//               bgColor: 'white',
+//               color: 'primary.500',
+//               fontWeight: 'bold',
+//             }}
+//           >
+//             환경 설정
+//           </Button>
+//         </ChakraLink>
+//       </HStack>
+//     </Flex>
+//   );
+// };
+
+const CustomTab = function ({ children, to }) {
   return (
-    <Flex
+    <ChakraLink as={ReactRouterLink} to={to} width={'full'}>
+      <Tab
+        width={'full'}
+        py={'4'}
+        bgColor={'primary.500'}
+        color={'white'}
+        border={'none'}
+        _selected={{
+          bgColor: 'white',
+          color: 'primary.500',
+          fontWeight: 'bold',
+        }}
+      >
+        {children}
+      </Tab>
+    </ChakraLink>
+  );
+};
+
+const SideBar = function () {
+  return (
+    <Tabs
       height="100vh"
       width="40"
       color="white"
       bgColor="primary.500"
       py="4"
       direction="column"
-      justifyContent="space-between"
-      alignItems="center"
       borderRightRadius="md"
       position="absolute"
       top="0"
       left="0"
       zIndex={2}
+      variant="enclosed-colored"
+      size={'lg'}
+      orientation="vertical"
+      defaultIndex={1}
     >
-      <VStack width="full" py="2">
-        {user ? (
-          <ChakraLink
-            as={ReactRouterLink}
-            to="/mypage"
-            textDecorationLine="none"
-            _hover={{ textDecoration: 'none' }}
-          >
-            <Avatar src={user?.photoURL} size="xl" />
-            <Text
-              mt={2}
-              fontSize="xl"
-              fontWeight="bold"
-              textAlign="center"
-            >
-              {user?.displayName}님
-            </Text>
-          </ChakraLink>
-        ) : (
-          <>
-            <SkeletonCircle size="24" />
-            <SkeletonText
-              mt={2}
-              skeletonHeight="5"
-              width="16"
-              noOfLines={1}
-            />
-          </>
-        )}
-      </VStack>
-
-      <VStack width="full" gap={0}>
-        {menuNames.map(menu => (
-            <ChakraLink
-              as={ReactRouterLink}
-              to={menu.path}
-              width="full"
-              key={menu.name}
-            >
-              <Button
-                width="full"
-                size="lg"
-                borderRadius="none"
-                color="white"
-                bgColor="primary.500"
-                fontSize="xl"
-                _hover={{
-                  bgColor: 'primary.700',
-                }}
-                _focus={{
-                  bgColor: 'white',
-                  color: 'primary.500',
-                  fontWeight: 'bold',
-                }}
-              >
-                {menu.name}
-              </Button>
-            </ChakraLink>
-          ))}
-      </VStack>
-      <HStack width="full">
-        <ChakraLink as={ReactRouterLink} to="/" width="full">
-          <Button
-            leftIcon={<FaCog />}
-            variant="ghost"
-            width="full"
-            size="lg"
-            borderRadius="none"
-            color="white"
-            bgColor="primary.500"
-            _hover={{
-              bgColor: 'primary.700',
-            }}
-            _focus={{
-              bgColor: 'white',
-              color: 'primary.500',
-              fontWeight: 'bold',
-            }}
-          >
-            환경 설정
-          </Button>
-        </ChakraLink>
-      </HStack>
-    </Flex>
+      <TabList
+        width={'full'}
+        display={'flex'}
+        flexDirection={'column'}
+        justifyContent={'space-between'}
+        alignItems={'center'}
+        margin={0}
+      >
+        <CustomTab to={'my-page'}>프로필</CustomTab>
+        <VStack width={'full'} justifyContent={'center'} gap={'0'}>
+          <CustomTab to={''}>메인화면</CustomTab>
+          <CustomTab to={'appointment'}>진료예약</CustomTab>
+          <CustomTab to={'appointment-history'}>진료내역</CustomTab>
+          <CustomTab to={'health-history'}>건강기록</CustomTab>
+          <CustomTab to={'medicines'}>복약관리</CustomTab>
+        </VStack>
+        <CustomTab to={'settings'}>설정</CustomTab>
+      </TabList>
+    </Tabs>
   );
 };
 
