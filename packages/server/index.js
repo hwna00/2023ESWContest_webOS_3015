43ef682074b8b62cf6d3a0b0f1329cf8db6b4a22
hwna00 <layton3534@gmail.com>
2023-09-20 01:19:23 +0900

    Feat(server): 네이버 로그인 callback 과정 처리
    - '/naver-callback'으로 들어오는 GET 요청에 대한 처리 추가
    - dotenv를 통해 환경변수 지정
    - 네이버 access_token을 통해 사용자 정보를 불러오는 기능 구현

--- packages/server/index.js
+++ packages/server/index.js
@@ -7,7 +7,7 @@ const fbAdmin = require('./config/fbAdmin');
 require('dotenv').config();
 
 const app = express();
-const port = 3000; //TODO: .env 파일의 PORT 이름 DBPORT 등으로 수정하기
+const port = 3000 || process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -82,8 +82,40 @@ app.get('/api/users/me', (req, res) => {
   res.json({});
 });
 
-app.get('/', (req, res) => {
-  console.log('get /');
+app.get('/naver-callback', async (req, res) => {
+  code = req.query.code;
+  state = req.query.state;
+  const redirectURI = `http://localhost:3000/naver-callback`;
+
+  api_url =
+    'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=' +
+    process.env.REACT_APP_NAVER_CLIENT_ID +
+    '&client_secret=' +
+    process.env.REACT_APP_NAVER_CLIENT_SECRET +
+    '&redirect_uri=' +
+    redirectURI +
+    '&code=' +
+    code +
+    '&state=' +
+    state;
+
+  const response = await axios.get(api_url, {
+    headers: {
+      'X-Naver-Client-Id': process.env.REACT_APP_NAVER_CLIENT_ID,
+      'X-Naver-Client-Secret': process.env.REACT_APP_NAVER_CLIENT_SECRET,
+    },
+  });
+  const {
+    data: { access_token },
+  } = response;
+
+  const userProfile = await axios.get('https://openapi.naver.com/v1/nid/me', {
+    headers: {
+      Authorization: 'Bearer ' + access_token,
+    },
+  });
+
+  console.log(userProfile);
 });
 
 app.listen(port, () => {
