4b7fc68681130ec3be08e31e7b617b32d4912df3
hwna00 <layton3534@gmail.com>
2023-09-20 13:01:56 +0900

    Feat(server): 네이버로부터 사용자 정보 받아오는 로직 추가
    - 네이버로부터 받은 code 값과 state 값을 이용하여 access_token을 요청
    - access_token을 이용하여 사용자의 정보를 요청

--- packages/apps/user/src/components/NaverLoginButton/NaverLoginButton.js
+++ packages/apps/user/src/components/NaverLoginButton/NaverLoginButton.js
@@ -3,10 +3,10 @@ import logo from '../../images/btnG_icon_circle.png';
 import { useCallback } from 'react';
 
 const NaverForm = function () {
-  const base_url = 'https://nid.naver.com/oauth2.0/authorize';
+  const BASE_URL = 'https://nid.naver.com/oauth2.0/authorize';
   const REDIRECT_URI = `http://localhost:3000/naver-callback`;
   const STATE = 'slfjsdf';
-  const NAVER_AUTH_URL = `${base_url}?response_type=code&client_id=${
+  const NAVER_AUTH_URL = `${BASE_URL}?response_type=code&client_id=${
     process.env.REACT_APP_NAVER_CLIENT_ID
   }&redirect_uri=${encodeURI(REDIRECT_URI)}&state=${STATE}`;
 
