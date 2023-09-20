0015b566f055b362521c7eb821ee1478420f9047
1004jaein <jsja031017@kookmin.ac.kr>
2023-09-20 19:08:30 +0900

    Feat : 병원 데스크 페이지 제작 준비

---
+++ packages/apps/hospital-desk/src/router.js
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from 'react-router-dom';
+import SignUp from './views/SignUp';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <SignUp />,
+    children: [],
+  },
+]);
+
+export default router;
---
+++ packages/apps/hospital-desk/src/views/SignUp.js
@@ -0,0 +1,11 @@
+import { FormControl, Input } from '@chakra-ui/react';
+
+const SignUp = function () {
+  return (
+    <FormControl>
+      <Input />
+    </FormControl>
+  );
+};
+
+export default SignUp;
