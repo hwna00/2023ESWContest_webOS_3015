b29b1f9b4aae623718cf42b6226185cbd5a0e913
hwna00 <layton3534@gmail.com>
2023-09-25 16:47:00 +0900

    Feat(server): 네이버 로그인 리디렉트 URL 변경
    - 기존: '/naver-callback'
    - 변경: '/api/auth/naver-callback'

--- packages/server/index.js
+++ packages/server/index.js
@@ -83,7 +83,7 @@ app.get('/api/users/me', (req, res) => {
 });
 
 const getNaverAuthApiUri = (code, state) => {
-  const REDIRECT_URI = 'http://localhost:3000/naver-callback';
+  const REDIRECT_URI = 'http://localhost:3000/api/auth/naver-callback';
 
   return (
     'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=' +
@@ -99,7 +99,7 @@ const getNaverAuthApiUri = (code, state) => {
   );
 };
 
-app.get('/naver-callback', async (req, res) => {
+app.get('/api/auth/naver-callback', async (req, res) => {
   const { code, state } = req.query;
   const API_URI = getNaverAuthApiUri(code, state);
 
