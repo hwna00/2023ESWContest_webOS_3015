ca74176834fd2f784778d898eedbaad365232807
hwna00 <layton3534@gmail.com>
2023-09-25 14:40:43 +0900

    Feat(server): 파이어베이스 커스텀 토큰 제작 함수 생성
    - fbCreateCustomToken
    
    ref: #67

--- packages/server/index.js
+++ packages/server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const fbAdmin = require('./config/fbAdmin');
 
 const app = express();
 const port = 3000; //TODO: .env 파일의 PORT 이름 DBPORT 등으로 수정하기
@@ -8,13 +9,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: 'http://localhost:8080' }));
 
-app.get('/', (req, res) => {
-  console.log('get /');
-});
+const fbCreateCustomToken = uid => {
+  fbAdmin
+    .auth()
+    .createCustomToken(uid)
+    .then(customToken => {
+      return customToken;
+    })
+    .catch(error => {
+      console.log('Error creating custom token:', error);
+    });
+};
 
-app.post('/api/auth/create-user', (req, res) => {
-  console.log(req.body);
-  //TODO: firebase로부터 온 요쳥이라면 JWT 토큰 생성하기
+app.get('/api/users/me', (req, res) => {
+  console.log('me');
+  //TODO: DB로부터 사용자 정보 검색
+  res.json({});
 });
 
 app.listen(port, () => {
