18829c88712f666de082c60b0da7978ab1a14b6c
hwna00 <layton3534@gmail.com>
2023-09-25 18:59:22 +0900

    Fix(server): 코드 중복 제거

--- packages/server/index.js
+++ packages/server/index.js
@@ -82,23 +82,6 @@ app.get('/api/users/me', (req, res) => {
   res.json({});
 });
 
-const getNaverAuthApiUri = (code, state) => {
-  const REDIRECT_URI = 'http://localhost:3000/api/auth/naver-callback';
-
-  return (
-    'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=' +
-    process.env.REACT_APP_NAVER_CLIENT_ID +
-    '&client_secret=' +
-    process.env.REACT_APP_NAVER_CLIENT_SECRET +
-    '&redirect_uri=' +
-    REDIRECT_URI +
-    '&code=' +
-    code +
-    '&state=' +
-    state
-  );
-};
-
 app.get('/api/auth/naver-callback', async (req, res) => {
   const { code, state } = req.query;
   const API_URI = getNaverAuthApiUri(code, state);
