6ffe39524bb9016809af3acb9fb172aae1586667
1004jaein <jsja031017@kookmin.ac.kr>
2023-09-29 21:05:30 +0900

    Feat : 예약 전체보기 페이지 제작 - 캘린더 삽입 - 날짜별 해당하는 예약만 보여주도록 설정

---
+++ packages/apps/hospital-desk/src/views/ViewAppointment/ViewAppointment.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+
+import { AiFillPlusCircle } from 'react-icons/ai';
+import dayjs from 'dayjs';
+import 'react-calendar/dist/Calendar.css';
+import styles from '@housepital/common/css/HideScrollBar.module.css';
+import { Box, Button, HStack, Heading, Text, VStack } from '@chakra-ui/react';
+
+import { ConfirmedReservation } from '../MainPage/Data';
+import TableRow from '../../component/TableSection/TableRow';
+import TableHeader from '../../component/TableSection/TableHeader';
+import Calendar from '../../component/Calendar/Calendar';
+
+function ViewAppointment() {
+  const [value, onChange] = useState(new Date());
+  const [day, setDay] = useState(new Date());
+
+  const filteredReservations = ConfirmedReservation.filter(reservation =>
+    dayjs(reservation.date_time).isSame(day, 'day'),
+  );
+  return (
+    <VStack p="8" spacing="8" alignItems="initial" w="100%">
+      <HStack justifyContent="space-between">
+        <Heading textAlign="left" p="4" fontSize="30px">
+          예약 전체보기
+        </Heading>
+        <Button colorScheme="primary" leftIcon={<AiFillPlusCircle />}>
+          예약추가
+        </Button>
+      </HStack>
+      <HStack
+        justifyItems="initial"
+        alignItems="unset"
+        justifyContent="space-between"
+      >
+        <Box w="480px" h="870px" border="1px" borderColor="black">
+          <Calendar
+            selectedDay={day}
+            setSelectedDay={setDay}
+            onChange={onChange}
+            value={value}
+          />
+        </Box>
+        <VStack w="700px" alignItems="unset">
+          <TableHeader
+            tableHeaders={['이름', '예약시간', '타입', '담당의사', '상세정보']}
+          />
+          <div className={styles.hideScrollBar}>
+            <Box maxH="830px" overflowY="scroll" w="100%">
+              {filteredReservations.map(reservation => (
+                <TableRow
+                  key={reservation.uid}
+                  data={reservation}
+                  first={reservation.name}
+                  second={reservation.date_time}
+                  third={
+                    reservation.is_NFTF ? (
+                      <Text>대면</Text>
+                    ) : (
+                      <Text>비대면</Text>
+                    )
+                  }
+                  fourth={reservation.doctorName}
+                  fifth="detail"
+                />
+              ))}
+            </Box>
+          </div>
+        </VStack>
+      </HStack>
+    </VStack>
+  );
+}
+
+export default ViewAppointment;
