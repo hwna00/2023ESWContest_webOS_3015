14c65750e546c289b9ef2571c8ef76863cc2c748
1004jaein <jsja031017@kookmin.ac.kr>
2023-10-03 16:06:14 +0900

    Feat : 조건에 맞는 예약 필터기능 추가

--- packages/apps/hospital-desk/src/views/ViewAppointment/ViewAppointment.js
+++ packages/apps/hospital-desk/src/views/ViewAppointment/ViewAppointment.js
@@ -33,31 +33,62 @@ function ViewAppointment() {
   const [value, onChange] = useState(new Date());
   const [day, setDay] = useState(new Date());
   const [filter, setFilter] = useState('viewAll');
-  const [filterType, setFilterType] = useState('FTF');
+  const [filterType, setFilterType] = useState('All');
+  const [selectedHour, setSelectedHour] = useState();
+  const [selectedMinute, setSelectedMinute] = useState();
+  const [selectedDoctor, setSelectedDoctor] = useState();
+  const [selectedState, setSelectedState] = useState();
 
   const { data } = useQuery(['appointments'], getAppointments);
 
   let filteredReservations = [];
   let uniqueDoctors = [];
   let uniqueHours = [];
+
   if (data) {
-    filteredReservations = data.filter(reservation =>
-      dayjs(reservation.date_time).isSame(day, 'day'),
-    );
-    uniqueDoctors = Array.from(
-      data.reduce(
-        (map, reservation) =>
-          map.set(reservation.doctor_id, reservation.doctorName),
-        new Map(),
-      ),
-    ).map(([doctor_id, doctorName]) => ({ doctor_id, doctorName }));
-    uniqueHours = Array.from(
-      data.reduce(
-        (set, reservation) =>
-          set.add(dayjs(reservation.date_time).format('HH')),
-        new Set(),
-      ),
-    );
+    if (filter === 'viewAll') {
+      filteredReservations = data.filter(reservation =>
+        dayjs(reservation.date_time).isSame(day, 'day'),
+      );
+    } else if (filter === 'viewSelection') {
+      filteredReservations = data.filter(reservation => {
+        if (!dayjs(reservation.date_time).isSame(day, 'day')) return false;
+
+        const reservationDateTime = dayjs(reservation.date_time);
+
+        if (selectedHour && reservationDateTime.format('HH') !== selectedHour)
+          return false;
+
+        if (
+          selectedMinute &&
+          reservationDateTime.format('mm') !== selectedMinute
+        )
+          return false;
+
+        if (selectedState && reservation.state_id !== selectedState)
+          return false;
+
+        if (selectedDoctor && reservation.doctor_id !== selectedDoctor)
+          return false;
+        if (filterType === 'FTF') return reservation.is_NFTF === false;
+        if (filterType === 'NFTF') return reservation.is_NFTF === true;
+        return true;
+      });
+      uniqueDoctors = Array.from(
+        data.reduce(
+          (map, reservation) =>
+            map.set(reservation.doctor_id, reservation.doctorName),
+          new Map(),
+        ),
+      ).map(([doctor_id, doctorName]) => ({ doctor_id, doctorName }));
+      uniqueHours = Array.from(
+        data.reduce(
+          (set, reservation) =>
+            set.add(dayjs(reservation.date_time).format('HH')),
+          new Set(),
+        ),
+      );
+    }
   }
 
   return (
@@ -96,44 +127,71 @@ function ViewAppointment() {
             <VStack alignItems="initial" p="6" spacing="2">
               <Radio value="viewAll">전체보기</Radio>
               <Radio value="viewSelection">선택보기</Radio>
-              <RadioGroup onChange={setFilterType} value={filterType}>
-                <HStack>
-                  <Radio value="FTF">대면</Radio>
-                  <Radio value="NFTF">비대면</Radio>
-                </HStack>
-                <SimpleGrid columns={2} spacing={4}>
-                  <Select placeholder="시간">
-                    {uniqueHours.map(hour => (
-                      <option key={hour}>{hour}</option>
-                    ))}
-                  </Select>
-                  <Select placeholder="분">
-                    <option>00분</option>
-                    <option>30분</option>
-                  </Select>
-                  <Select placeholder="예약상태">
-                    <option key="aw" value="aw">
-                      예약 미확정
-                    </option>
-                    <option key="ac" value="ac">
-                      예약 확정
-                    </option>
-                    <option key="dc" value="dc">
-                      진료 완료
-                    </option>
-                    <option key="pc" value="pc">
-                      결제 완료
-                    </option>
-                  </Select>
-                  <Select placeholder="담당의사">
-                    {uniqueDoctors.map(doctor => (
-                      <option key={doctor.doctor_id}>
-                        {doctor.doctorName}
+              {filter === 'viewSelection' && (
+                <>
+                  <RadioGroup onChange={setFilterType} value={filterType}>
+                    <HStack>
+                      <Radio value="ALl">모두 보기</Radio>
+                      <Radio value="FTF">대면</Radio>
+                      <Radio value="NFTF">비대면</Radio>
+                    </HStack>
+                  </RadioGroup>
+                  <SimpleGrid columns={2} spacing={4}>
+                    <Select
+                      placeholder="시"
+                      value={selectedHour}
+                      onChange={e => setSelectedHour(e.target.value)}
+                    >
+                      {uniqueHours.map(hour => (
+                        <option key={hour} value={hour}>
+                          {hour}
+                        </option>
+                      ))}
+                    </Select>
+                    <Select
+                      placeholder="분"
+                      value={selectedMinute}
+                      onChange={e => setSelectedMinute(e.target.value)}
+                    >
+                      <option key="00" value="00">
+                        00분
                       </option>
-                    ))}
-                  </Select>
-                </SimpleGrid>
-              </RadioGroup>
+                      <option key="30" value="30">
+                        30분
+                      </option>
+                    </Select>
+                    <Select
+                      placeholder="예약 상태"
+                      value={selectedState}
+                      onChange={e => setSelectedState(e.target.value)}
+                    >
+                      <option key="aw" value="aw">
+                        예약 미확정
+                      </option>
+                      <option key="ac" value="ac">
+                        예약 확정
+                      </option>
+                      <option key="dc" value="dc">
+                        진료 완료
+                      </option>
+                      <option key="pc" value="pc">
+                        결제 완료
+                      </option>
+                    </Select>
+                    <Select
+                      placeholder="담당 의사"
+                      value={selectedDoctor}
+                      onChange={e => setSelectedDoctor(e.target.value)}
+                    >
+                      {uniqueDoctors.map(doctor => (
+                        <option key={doctor.doctor_id} value={doctor.doctor_id}>
+                          {doctor.doctorName}
+                        </option>
+                      ))}
+                    </Select>
+                  </SimpleGrid>
+                </>
+              )}
             </VStack>
           </RadioGroup>
         </Box>
