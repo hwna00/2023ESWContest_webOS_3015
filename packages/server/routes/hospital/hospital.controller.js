52992af84e1dbf0ac01a262b674044325c6d75e5
yangjiwoong <wpyang123@naver.com>
2023-10-08 14:10:26 +0900

    Feat(server): 병원 상세 정보 조회 REST API 생성
    - 병원 상세 정보 조회 GET API 생성
    
    ref: #97

--- packages/server/routes/hospital/hospital.controller.js
+++ packages/server/routes/hospital/hospital.controller.js
@@ -159,3 +159,70 @@ exports.readAppointments = async function (req, res) {
     });
   }
 };
+
+exports.readHospitalQuery = async function (connection, hospitalId) {
+  const Query = `SELECT
+  H.hospital_id,
+  H.name,
+  H.ykiho,
+  H.description,
+  JSON_ARRAYAGG(
+      JSON_OBJECT(
+          'id', D.doctor_id,
+          'name', D.name,
+          'specialty', D.specialty,
+          'fields', D.fields,
+          'rate', ifnull((SELECT rate FROM DoctorRate WHERE doctor_id = D.doctor_id),0)
+      )
+  ) AS doctors
+FROM Hospitals H
+JOIN Doctors D ON H.hospital_id = ? AND D.hospital_id = H.hospital_id
+`;
+
+  const Params = [hospitalId];
+
+  const rows = await connection.query(Query, Params);
+
+  return rows;
+};
+
+exports.readHospital = async function (req, res) {
+  const { hospitalId } = req.params;
+  try {
+    const connection = await pool.getConnection(async conn => conn);
+    try {
+      const [rows] = await exports.readHospitalQuery(connection, hospitalId);
+      if (rows.length === 0) {
+        throw Error('Hospital not found');
+      } else {
+        return res.json({
+          result: rows[0],
+          isSuccess: true,
+          code: 200,
+          message: '병원 조회 성공',
+        });
+      }
+    } catch (err) {
+      if (err.message === 'Hospital not found') {
+        return res.json({
+          isSuccess: false,
+          code: 404,
+          message: '병원을 찾을 수 없습니다.',
+        });
+      }
+      return res.json({
+        isSuccess: false,
+        code: 500,
+        message: '서버 오류',
+      });
+    } finally {
+      connection.release();
+    }
+  } catch (err) {
+    return res.json({
+      isSuccess: false,
+      code: 500,
+      message: '데이터베이스 연결 실패',
+    });
+  }
+};
