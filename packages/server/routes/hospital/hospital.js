c65ca6c311836120c5349114016b107df976e40d
yangjiwoong <wpyang123@naver.com>
2023-10-09 20:32:55 +0900

    Style(server): 미들웨어 함수명 변경
    - 미드뤠어 함수명을 규칙있게 변경

--- packages/server/routes/hospital/hospital.js
+++ packages/server/routes/hospital/hospital.js
@@ -4,7 +4,10 @@ const hospitalMid = require('./hospital.controller');
 
 router.get('/hospitals', hospitalMid.readHospitals);
 router.get('/hospitals/:hospitalId', hospitalMid.readHospital);
-router.get('/hospitals/:hospitalId/appointments', hospitalMid.readAppointments);
+router.get(
+  '/hospitals/:hospitalId/appointments',
+  hospitalMid.readHospitalAppointments,
+);
 router.post('/hospitals', hospitalMid.createHospital);
 
 module.exports = router;
