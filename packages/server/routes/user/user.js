300416d935ec0b3aca852c641536cf25b39ca735
yangjiwoong <wpyang123@naver.com>
2023-10-09 21:44:27 +0900

    Feat(server): 특정 유저의 예약 정보를 모두 가져오는 REST API 작성
    - 특정 유저의 예약 정보를 모두 가져오는 GET API 작성
    - [TODO] user_id가 DB에 존재하지 않을 때와, 존재하지만 예약정보가 없을 때에 대한 구분 필요
    
    ref: #138

--- packages/server/routes/user/user.js
+++ packages/server/routes/user/user.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const userMid = require('./user.controller');
 
 router.get('/users/:uid', userMid.readUser);
+router.get('/users/:uid/appointments', userMid.readUserAppointments);
 router.post('/users', userMid.createUser);
 router.patch('/users/:uid', userMid.updateUser);
 
