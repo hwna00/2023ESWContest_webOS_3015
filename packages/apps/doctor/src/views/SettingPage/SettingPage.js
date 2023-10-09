a3f73529ae32323a26bb49f10dc0afa7e7c6c8d9
hwna00 <layton3534@gmail.com>
2023-10-09 12:51:07 +0900

    Feat(doctor): 설정 페이지 제작
    - 로그아웃 기능 추가

--- packages/apps/doctor/src/views/SettingPage/SettingPage.js
+++ packages/apps/doctor/src/views/SettingPage/SettingPage.js
@@ -1,5 +1,39 @@
+import { useCallback } from 'react';
+
+import { Button, Heading, ListItem, UnorderedList } from '@chakra-ui/react';
+
+import { fbLogOut } from '../../../firebase';
+
 const SettingPage = function () {
-  return 'SettingPage';
+  // TODO: webOS API 사용해서 앱 종료 기능 추가하기
+  // TODO: 계정 전환 기능 추가하기
+
+  const onLogoutClick = useCallback(() => {
+    fbLogOut();
+  }, []);
+
+  return (
+    <>
+      <Heading>설정</Heading>
+      <UnorderedList listStyleType="none" spacing="4" mt="8">
+        <ListItem>
+          <Button
+            variant="outline"
+            colorScheme="red"
+            size="lg"
+            onClick={onLogoutClick}
+          >
+            로그아웃
+          </Button>
+        </ListItem>
+        <ListItem>
+          <Button colorScheme="red" size="lg">
+            앱 종료
+          </Button>
+        </ListItem>
+      </UnorderedList>
+    </>
+  );
 };
 
 export default SettingPage;
