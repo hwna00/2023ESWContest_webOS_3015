0f4e16a6be8e7e97af09f1a2f4a1509ea08bbcc6
hwna00 <layton3534@gmail.com>
2023-10-10 19:49:19 +0900

    Feat(user): 진료 예약 화면과 예약 api 연결
    - 사용자의 현재 예약 리스트를 받아오고, 그 길이를 진료 현황 화면에 보여주는 기능 추가

--- packages/apps/user/src/views/Appointment/Appointment.js
+++ packages/apps/user/src/views/Appointment/Appointment.js
@@ -11,8 +11,37 @@ import {
 
 import AppointmentViewList from '../../components/AppointmentViewList/AppointmentViewList';
 import { FavoriteList } from './dataList';
+import { useCallback, useEffect, useState } from 'react';
+import { getAppointments } from '../../api';
+import { useSelector } from 'react-redux';
 
 const Appointment = function () {
+  const uid = useSelector(state => state.me.uid);
+  const [appointments, setAppointments] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  const fetchAppointments = useCallback(async () => {
+    if (!uid) {
+      setAppointments();
+      setIsLoading(true);
+      return;
+    }
+    try {
+      const response = await getAppointments(uid);
+      setAppointments(response.length);
+      setIsError(false);
+    } catch {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [uid]);
+
+  useEffect(() => {
+    fetchAppointments();
+  }, [fetchAppointments]);
+
   return (
     <VStack w="full" h="full" justifyContent={'space-between'} gap={'6'}>
       <HStack w="full" gap="6" px="2">
@@ -23,7 +52,28 @@ const Appointment = function () {
             </Text>
           </Box>
           <Flex h="75%" alignItems="center" justifyContent="center">
-            <Text opacity="0.5">예약 내역이 없습니다.</Text>
+            {isLoading ? (
+              '예약을 불러오는 중입니다.'
+            ) : (
+              <>
+                {isError && <Text opacity="0.5">예약 내역이 없습니다.</Text>}
+                {
+                  <ChakraLink
+                    as={ReactRouterLink}
+                    to="waiting-room"
+                    textDecoration="none !important"
+                  >
+                    <Text
+                      textAlign="center"
+                      textDecoration="underline"
+                      fontSize="lg"
+                    >
+                      {appointments}건의 예약이 있습니다.
+                    </Text>
+                  </ChakraLink>
+                }
+              </>
+            )}
           </Flex>
         </Box>
 
