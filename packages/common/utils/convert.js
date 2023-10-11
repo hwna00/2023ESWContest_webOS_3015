de678b37509c0f9fbf561dc378c97bc59a8963ad
1004jaein <jsja031017@kookmin.ac.kr>
2023-10-11 20:42:27 +0900

    Feat : 요일 별 진료시간 변환

--- packages/common/utils/convert.js
+++ packages/common/utils/convert.js
@@ -1,6 +1,73 @@
 import { fetchResultsTime } from '@housepital/hospital-desk/src/api';
 
-const convert = async ykiho => {
+const formatTime = time => {
+  const hours = Math.floor(time / 100);
+  const minutes = time % 100;
+
+  return `${hours.toString().padStart(2, '0')}:${minutes
+    .toString()
+    .padStart(2, '0')}`;
+};
+
+const addTime = timeTable => {
+  const timeList = {
+    mon: [],
+    tue: [],
+    wed: [],
+    thu: [],
+    fri: [],
+    sat: [],
+    sun: [],
+  };
+
+  Object.keys(timeList).forEach(day => {
+    if (!(day === 'sun')) {
+      const startKey = `trmt${day.charAt(0).toUpperCase() + day.slice(1)}Start`;
+      const endKey = `trmt${day.charAt(0).toUpperCase() + day.slice(1)}End`;
+
+      let startTime = parseInt(timeTable.treatmentHours[startKey], 10);
+      const endTime = parseInt(timeTable.treatmentHours[endKey], 10);
+
+      let lunchStartTime;
+      let lunchEndTime;
+
+      if (day !== 'sat' && timeTable.treatmentHours.lunchWeekStart) {
+        lunchStartTime = parseInt(
+          timeTable.treatmentHours.lunchWeekStart.replace(':', ''),
+          10,
+        );
+        lunchEndTime = parseInt(
+          timeTable.treatmentHours.lunchWeekEnd.replace(':', ''),
+          10,
+        );
+      } else if (timeTable.treatmentHours.lunchSatStart) {
+        lunchStartTime = parseInt(
+          timeTable.treatmentHours.lunchSatStart.replace(':', ''),
+          10,
+        );
+        lunchEndTime = parseInt(
+          timeTable.treatmentHours.lunchSatEnd.replace(':', ''),
+          10,
+        );
+      }
+
+      while (startTime < endTime) {
+        if (!(startTime >= lunchStartTime && startTime < lunchEndTime)) {
+          timeList[day].push(formatTime(startTime));
+        }
+        startTime += 30;
+
+        if (startTime % 100 === 60) {
+          startTime += 40;
+        }
+      }
+    }
+  });
+
+  return timeList;
+};
+
+const getDtlInfo = async ykiho => {
   const data = await fetchResultsTime(ykiho);
   const { item } = data.response.body.items;
   const [rcvSatStart, rcvSatEnd] = item.rcvSat.split(' - ');
@@ -40,11 +107,13 @@ const convert = async ykiho => {
       trmtFriEnd: String(item.trmtFriEnd),
       trmtSatStart: String(item.trmtSatStart),
       trmtSatEnd: String(item.trmtSatEnd),
+      trmtSunStart: String(item.trmtSunStart),
+      trmtSunEnd: String(item.trmtSunEnd),
       noTrmtHoli: item.noTrmtHoli,
       noTrmtSun: item.noTrmtSun,
     },
   };
-  return timeTable;
+  return addTime(timeTable);
 };
 
-export default convert;
+export default getDtlInfo;
