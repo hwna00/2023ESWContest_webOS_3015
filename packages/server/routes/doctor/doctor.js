39eccb80f22e4c8a3fc141716334b8fcfd8eb305
yangjiwoong <wpyang123@naver.com>
2023-10-16 21:05:14 +0900

    Feat(server): 의사가 환자들의 모든 진료 기록을 가져오는 REST API 생성
    - 의사가 환자들의 모든 진료 기록을 가져오는 GET API 생성
    
    ref: #194

--- packages/server/routes/doctor/doctor.js
+++ packages/server/routes/doctor/doctor.js
@@ -6,5 +6,6 @@ router.post('/doctors', doctorMid.createDoctor);
 router.get('/doctors', doctorMid.readDoctors);
 router.get('/doctors/:doctorId', doctorMid.readDoctor);
 router.get('/doctors/:doctorId/appointments', doctorMid.readDoctorAppointments);
+router.get('/doctors/:doctorId/diagnoses', doctorMid.readDoctorDiagnoses);
 
 module.exports = router;
