99ab296b43f66e2f0f4581e47264bd178b3753b6
1004jaein <jsja031017@kookmin.ac.kr>
2023-10-16 20:56:41 +0900

    Feat :  프로필 이미지 css 수정

--- packages/apps/center119/src/views/RequestDetail.jsx
+++ packages/apps/center119/src/views/RequestDetail.jsx
@@ -23,74 +23,74 @@ function RequestDetail() {
   console.log(data);
   return (
     <>
-      {/* {isLoading ? (
-        <LoadingPage />
-      ) : ( */}
-      <Box overflow="hidden">
-        <HStack justifyContent="space-between" alignItems="center">
-          <Heading>{data.name}</Heading>
-          <HStack>
-            <Button
-              colorScheme="red"
-              variant="outline"
-              onClick={() => updateRequestState(id, 'rj', '')}
-            >
-              상담취소
-            </Button>
-            <Button
-              colorScheme="primary"
-              variant="outline"
-              onClick={() => updateRequestState(id, 'rc', null)}
-            >
-              상담완료
-            </Button>
+      {isLoading ? (
+        <Text>로딩 중</Text>
+      ) : (
+        <Box overflow="hidden">
+          <HStack justifyContent="space-between" alignItems="center">
+            <Heading>{data.name}</Heading>
+            <HStack>
+              <Button
+                colorScheme="red"
+                variant="outline"
+                onClick={() => updateRequestState(id, 'rj', '')}
+              >
+                상담취소
+              </Button>
+              <Button
+                colorScheme="primary"
+                variant="outline"
+                onClick={() => updateRequestState(id, 'rc', null)}
+              >
+                상담완료
+              </Button>
+            </HStack>
           </HStack>
-        </HStack>
 
-        <Grid width="full" templateColumns="1fr 3fr 1fr" gap="8" mt="4">
-          <AspectRatio ratio={1} width="40">
-            <Avatar src="" alt={data.name} objectFit="cover" />
-          </AspectRatio>
+          <Grid width="full" templateColumns="1fr 3fr 1fr" gap="8" mt="4">
+            <AspectRatio ratio={1}>
+              <Avatar src="" alt={data.name} objectFit="cover" />
+            </AspectRatio>
 
-          <VStack justifyContent="center" alignItems="flex-start">
-            <Text fontSize="lg" fontWeight="bold">
-              이름 : {data?.name}
-            </Text>
-            <Text fontSize="lg" fontWeight="bold">
-              전화 번호 : {data?.phoneNumber}
-            </Text>
-            <Text fontSize="lg" fontWeight="bold">
-              생년월일 : {data?.birthDate}
-            </Text>
-            <Text fontSize="lg" fontWeight="bold">
-              주소 : {data?.address}
-            </Text>
-            <Text fontSize="lg" fontWeight="bold">
-              복용약 : {data?.regularMedicines}
-            </Text>
-            <Text fontSize="lg" fontWeight="bold">
-              만성질환 : {data?.chronicDisease}
-            </Text>
-          </VStack>
-        </Grid>
+            <VStack justifyContent="center" alignItems="flex-start">
+              <Text fontSize="lg" fontWeight="bold">
+                이름 : {data?.name}
+              </Text>
+              <Text fontSize="lg" fontWeight="bold">
+                전화 번호 : {data?.phoneNumber}
+              </Text>
+              <Text fontSize="lg" fontWeight="bold">
+                생년월일 : {data?.birthDate}
+              </Text>
+              <Text fontSize="lg" fontWeight="bold">
+                주소 : {data?.address}
+              </Text>
+              <Text fontSize="lg" fontWeight="bold">
+                복용약 : {data?.regularMedicines}
+              </Text>
+              <Text fontSize="lg" fontWeight="bold">
+                만성질환 : {data?.chronicDisease}
+              </Text>
+            </VStack>
+          </Grid>
 
-        <Box mt="6" height="full" overflowY="scroll">
-          <Box mt="6" height="full">
-            <Heading as="h2" fontSize="2xl" fontWeight="bold">
-              환자 전달 사항
-            </Heading>
-            <Textarea
-              defaultValue={data?.message}
-              height="full"
-              mt="4"
-              bgColor="primary.100"
-              borderRadius="md"
-              readOnly
-            />
+          <Box mt="6" height="full" overflowY="scroll">
+            <Box mt="6" height="full">
+              <Heading as="h2" fontSize="2xl" fontWeight="bold">
+                환자 전달 사항
+              </Heading>
+              <Textarea
+                defaultValue={data?.message}
+                height="full"
+                mt="4"
+                bgColor="primary.100"
+                borderRadius="md"
+                readOnly
+              />
+            </Box>
           </Box>
         </Box>
-      </Box>
-      {/* )} */}
+      )}
     </>
   );
 }
