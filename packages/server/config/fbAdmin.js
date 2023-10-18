f40a906859963a16c6f66ff748a72ed831fe0fbd
hwna00 <layton3534@gmail.com>
2023-10-18 21:04:58 +0900

    Fix(server) fbAdmin 주석 처리

--- packages/server/config/fbAdmin.js
+++ packages/server/config/fbAdmin.js
@@ -1,10 +1,10 @@
 const admin = require('firebase-admin');
 require('dotenv').config();
 
-const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH);
+// const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT_KEY_PATH);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
 
 module.exports = admin;
