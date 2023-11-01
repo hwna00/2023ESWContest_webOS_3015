451ee312fea126bd094861fde89d3b1c4c0c9195
yangjiwoong <wpyang123@naver.com>
2023-11-01 19:42:58 +0900

    Feat(server): 가장 최근에 측정한 활력징후 값을 가져오는 REST API 생성
    - GET 요청을 통해 가장 최근 측정한 활력징후 값을 가져온다.
    - 심박수와 체온을 모두 가져온다.
    
    ref: #275

--- packages/server/routes/user/user.js
+++ packages/server/routes/user/user.js
@@ -9,6 +9,7 @@ router.get('/users/:uid/appointments', userMid.readUserAppointments);
 router.get('/users/:uid/diagnoses', userMid.readUserDiagnoses);
 router.get('/users/:uid/medecines', userMid.readUserMedecines);
 router.get('/users/:uid/vital-signs', userMid.readUserVitalSigns);
+router.get('/users/:uid/recent-vital-signs', userMid.readUserRecentVitalSigns);
 router.get('/users/:uid/favorites', userMid.readUserFavorites);
 router.get('/users/:uid/side-effects', userMid.readUserSideEffects);
 router.patch('/users/:uid', userMid.updateUser);
