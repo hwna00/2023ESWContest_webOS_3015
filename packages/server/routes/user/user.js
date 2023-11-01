010f99d3bdd13da199454f2050569f26f2d7e94b
yangjiwoong <wpyang123@naver.com>
2023-11-01 17:31:26 +0900

    Feat(sever): 부작용 기록을 생성하는 REST API 생성
    - uid, expression, symptom, candidatePills를 저장한다.
    
    ref: #266

--- packages/server/routes/user/user.js
+++ packages/server/routes/user/user.js
@@ -2,13 +2,14 @@ const router = require('express').Router();
 
 const userMid = require('./user.controller');
 
+router.post('/users', userMid.createUser);
+router.post('/users/:uid/side-effects', userMid.createUserSideEffects);
 router.get('/users/:uid', userMid.readUser);
 router.get('/users/:uid/appointments', userMid.readUserAppointments);
 router.get('/users/:uid/diagnoses', userMid.readUserDiagnoses);
 router.get('/users/:uid/medecines', userMid.readUserMedecines);
 router.get('/users/:uid/vital-signs', userMid.readUserVitalSigns);
 router.get('/users/:uid/favorites', userMid.readUserFavorites);
-router.post('/users', userMid.createUser);
 router.patch('/users/:uid', userMid.updateUser);
 
 module.exports = router;
