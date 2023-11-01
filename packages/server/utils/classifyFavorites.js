aab8516d5e756e0ae4e4444800b982825a25d255
yangjiwoong <wpyang123@naver.com>
2023-11-01 18:29:51 +0900

    Hotfix(server): 즐겨찾기 정보를 모두 가져올 때 type값 수정
    - 즐겨찾기 정보를 모두 가져올 때 type값 없이 id만 넘겨주던 방식에서 type까지 넘겨주도록 수정

--- packages/server/utils/classifyFavorites.js
+++ packages/server/utils/classifyFavorites.js
@@ -7,10 +7,10 @@ module.exports = favooritesFromDB => {
   for (let i = 0; i < favooritesFromDB.length; i += 1) {
     switch (favooritesFromDB[i].type) {
       case 'doctor':
-        Favorites.doctors.push(favooritesFromDB[i].id);
+        Favorites.doctors.push(favooritesFromDB[i]);
         break;
       case 'hospital':
-        Favorites.hospitals.push(favooritesFromDB[i].id);
+        Favorites.hospitals.push(favooritesFromDB[i]);
         break;
       default:
         break;
